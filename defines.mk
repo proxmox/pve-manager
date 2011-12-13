7d06188a0404ad204fb11116f4b2659ef0a21615
Dietmar Maurer <dietmar@proxmox.com>
2011-12-13 10:19:02 +0100

    add HA resource agent

--- defines.mk
+++ defines.mk
@@ -2,13 +2,14 @@ RELEASE=2.0
 
 VERSION=2.0
 PACKAGE=pve-manager
-PACKAGERELEASE=14
+PACKAGERELEASE=15
 
 BINDIR=${DESTDIR}/usr/bin
 PERLLIBDIR=${DESTDIR}/usr/share/perl5
 MAN1DIR=${DESTDIR}/usr/share/man/man1
 CRONDAILYDIR=${DESTDIR}/etc/cron.daily
-INITDBINDIR=${DESTDIR}/etc/init.d/
+INITDBINDIR=${DESTDIR}/etc/init.d
+HARADIR=${DESTDIR}/usr/share/cluster
 DOCDIR=${DESTDIR}/usr/share/doc/${PACKAGE}
 PODDIR=${DESTDIR}/usr/share/doc/${PACKAGE}/pod
 WWWBASEDIR=${DESTDIR}/usr/share/${PACKAGE}
