efb38000c754baddfb0c005f3a4507271e4cf68c
Dietmar Maurer <dietmar@proxmox.com>
2012-02-10 08:38:06 +0100

    cleanups

--- www/manager/form/CompressionSelector.js
+++ www/manager/form/CompressionSelector.js
@@ -1,14 +1,25 @@
-Ext.define('PVE.form.CompressionSelector', {
+Ext.define('PVE.form.CPUModelSelector', {
     extend: 'PVE.form.KVComboBox',
-    alias: ['widget.pveCompressionSelector'],
+    alias: ['widget.CPUModelSelector'],
   
     initComponent: function() {
 	var me = this;
 
         me.data = [ 
-	    ['', 'none'],
-	    ['lzo', 'LZO (fast)'],
-	    ['gzip', 'GZIP (good)'],
+	    ['', 'Default (qemu64)'],
+	    ['486', '486'],
+	    ['athlon', 'athlon'],
+	    ['core2duo', 'core2duo'],
+	    ['coreduo', 'coreduo'],
+	    ['kvm32', 'kvm32'],
+	    ['kvm64', 'kvm64'],
+	    ['pentium', 'pentium'],
+	    ['pentium2', 'pentium2'],
+	    ['pentium3', 'pentium3'],
+	    ['phenom', 'phenom'],
+	    ['qemu32', 'qemu32'],
+	    ['qemu64', 'qemu64'],
+	    ['host', 'host']
 	];
 
 	me.callParent();
