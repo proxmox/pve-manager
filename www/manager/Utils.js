dd271a32c587b73d225c1e2543cd6dc913fb6f1b
Dietmar Maurer <dietmar@proxmox.com>
2013-05-10 06:33:55 +0200

    fix spelling

--- www/manager/Utils.js
+++ www/manager/Utils.js
@@ -440,7 +440,7 @@ Ext.define('PVE.Utils', { statics: {
 	qmdestroy: [ 'VM', gettext('Destroy') ],
 	qmigrate: [ 'VM', gettext('Migrate') ],
 	qmclone: [ 'VM', gettext('Clone') ],
-	qmtemplate: [ 'VM', gettext('Convert To Template') ],
+	qmtemplate: [ 'VM', gettext('Convert to template') ],
 	qmstart: [ 'VM', gettext('Start') ],
 	qmstop: [ 'VM', gettext('Stop') ],
 	qmreset: [ 'VM', gettext('Reset') ],
