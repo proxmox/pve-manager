eac3a06d2a195bb85ab82f802bc7c842649f562b
Dietmar Maurer <dietmar@proxmox.com>
2013-07-22 07:27:48 +0200

    apltest: ignore architecture

--- aplinfo/apltest.pl
+++ aplinfo/apltest.pl
@@ -13,8 +13,11 @@ foreach my $k (keys %{$pkglist->{'all'}}) {
     next if $k eq 'pve-web-news';
     my $res = $pkglist->{'all'}->{$k};
 
-    my $template = "$res->{os}-$res->{package}_$res->{version}_i386.tar.gz";
+    my $template = "$res->{os}-$res->{package}_$res->{version}_ARCH.tar.gz";
     $template =~ s/$res->{os}-$res->{os}-/$res->{os}-/;
+    
+    $k =~ s/_amd64\.tar\.gz$/_ARCH.tar.gz/;
+    $k =~ s/_i386\.tar\.gz$/_ARCH.tar.gz/;
 
     if ($k ne $template) {
 	print "ERROR: $k != $template\n";
