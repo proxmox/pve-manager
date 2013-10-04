proxmox/pve-manager

commit a107e264c997825a31904d2ffbed256161561b72

bump version to 3.1-16

PVE/VZDump.pm
@@ -971,7 +971,7 @@ sub exec_backup_task {
     eval { unlink $task->{tmptar} if $task->{tmptar} && -f $task->{tmptar}; };
     warn $@ if $@;
 
-    eval { rmtree $task->{tmpdir} if $task->{tmpdir} && -d $task->{tmpdir}; };
+#    eval { rmtree $task->{tmpdir} if $task->{tmpdir} && -d $task->{tmpdir}; };
     warn $@ if $@;
 
     my $delay = $task->{backuptime} = time () - $vmstarttime;

PVE/VZDump/OpenVZ.pm
@@ -303,8 +303,8 @@ sub cleanup {
 
     if ($task->{cleanup}->{etc_vzdump}) {
 	my $dir = "$task->{snapdir}/etc/vzdump";
-	eval { rmtree $dir if -d $dir; };
-	$self->logerr ($@) if $@;
+	#eval { rmtree $dir if -d $dir; };
+	#$self->logerr ($@) if $@;
     }
 
 }

defines.mk
@@ -2,7 +2,7 @@ RELEASE=3.1
 
 VERSION=3.1
 PACKAGE=pve-manager
-PACKAGERELEASE=15
+PACKAGERELEASE=16
 
 BINDIR=${DESTDIR}/usr/bin
 PERLLIBDIR=${DESTDIR}/usr/share/perl5

commit fe6643b6e97af3fcbac151647dc897bd3cb15b94

vzdump: remove temporary directories
This bug was introduced with commit a107e264c997825a31904d2ffbed256161561b72

PVE/VZDump.pm
@@ -971,7 +971,7 @@ sub exec_backup_task {
     eval { unlink $task->{tmptar} if $task->{tmptar} && -f $task->{tmptar}; };
     warn $@ if $@;
 
-#    eval { rmtree $task->{tmpdir} if $task->{tmpdir} && -d $task->{tmpdir}; };
+    eval { rmtree $task->{tmpdir} if $task->{tmpdir} && -d $task->{tmpdir}; };
     warn $@ if $@;
 
     my $delay = $task->{backuptime} = time () - $vmstarttime;

PVE/VZDump/OpenVZ.pm
@@ -303,8 +303,8 @@ sub cleanup {
 
     if ($task->{cleanup}->{etc_vzdump}) {
 	my $dir = "$task->{snapdir}/etc/vzdump";
-	#eval { rmtree $dir if -d $dir; };
-	#$self->logerr ($@) if $@;
+	eval { rmtree $dir if -d $dir; };
+	$self->logerr ($@) if $@;
     }
 
 }