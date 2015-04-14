6521c00bed438e3e8ea984e583c74779119ea210
Emmanuel Kasper <e.kasper@proxmox.com>
2015-04-14 13:28:20 +0200

    Change source attribute of javascript include to point to our manager5 directory.
    For ext 5 developpment, we will include in the html headers every javascript ressources
    incrementally, so we can bypass the "make install" step.
    
    Signed-off-by: Emmanuel Kasper <e.kasper@proxmox.com>

--- PVE/ExtJSIndex5.pm
+++ PVE/ExtJSIndex5.pm
@@ -26,7 +26,7 @@ _EOD
 
     $page .= <<_EOD;
     <script type="text/javascript" src="/pve2/ext5/ext-all-debug.js"></script>
-    <script type="text/javascript" src="/pve2/ext4/pvemanagerlib.js"></script>
+    <script type="text/javascript" src="/pve2/manager5/Utils.js"></script>
     <script type="text/javascript" src="/pve2/ext5/packages/ext-locale/build/ext-locale-${lang}.js"></script>
 _EOD
 
