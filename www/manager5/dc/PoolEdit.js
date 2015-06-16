0f524f77bb7724b51fdd9614a68291a391d9fd28
Dietmar Maurer <dietmar@proxmox.com>
2015-06-16 17:42:36 +0200

    copy dc/PoolEdit.js from manager to manager5

---
+++ www/manager5/dc/PoolEdit.js
@@ -0,0 +1,48 @@
+Ext.define('PVE.dc.PoolEdit', {
+    extend: 'PVE.window.Edit',
+    alias: ['widget.pveDcPoolEdit'],
+
+    initComponent : function() {
+        var me = this;
+
+        me.create = !me.poolid;
+
+        var url;
+        var method;
+
+        if (me.create) {
+            url = '/api2/extjs/pools';
+            method = 'POST';
+        } else {
+            url = '/api2/extjs/pools/' + me.poolid;
+            method = 'PUT';
+        }
+
+        Ext.applyIf(me, {
+            subject: gettext('Pool'),
+            url: url,
+            method: method,
+            items: [
+                {
+		    xtype: me.create ? 'pvetextfield' : 'displayfield',
+		    fieldLabel: gettext('Name'),
+		    name: 'poolid',
+		    value: me.poolid,
+		    allowBlank: false
+		},
+                {
+		    xtype: 'textfield',
+		    fieldLabel: gettext('Comment'),
+		    name: 'comment',
+		    allowBlank: true
+		}
+            ]
+        });
+
+        me.callParent();
+
+        if (!me.create) {
+            me.load();
+        }
+    }
+});
