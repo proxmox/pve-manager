c9ea843b1bd35eb60d934f869793043f6c61698d
Dietmar Maurer <dietmar@proxmox.com>
2015-06-19 12:40:18 +0200

    pveversion: also remove clvm

--- PVE/API2/APT.pm
+++ PVE/API2/APT.pm
@@ -534,9 +534,9 @@ __PACKAGE__->register_method({
 	}
 
 	# comment out old packages uses before 4.0
-	# resource-agents-pve fence-agents-pve vzctl vzprocps vzquota
+	# clvm resource-agents-pve fence-agents-pve vzctl vzprocps vzquota
 	
-	push @list, qw(lvm2 clvm corosync-pve libqb0 pve-cluster qemu-server pve-firmware libpve-common-perl libpve-access-control libpve-storage-perl pve-libspice-server1 vncterm pve-qemu-kvm pve-container pve-firewall pve-ha-manager ksm-control-daemon glusterfs-client lxc-pve lxcfs cgmanager);
+	push @list, qw(lvm2 corosync-pve libqb0 pve-cluster qemu-server pve-firmware libpve-common-perl libpve-access-control libpve-storage-perl pve-libspice-server1 vncterm pve-qemu-kvm pve-container pve-firewall pve-ha-manager ksm-control-daemon glusterfs-client lxc-pve lxcfs cgmanager);
 
 	my $pkglist = [];
 	
