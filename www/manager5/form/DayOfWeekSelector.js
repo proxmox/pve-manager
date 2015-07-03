f7fe82f8f14ce977594cf6f31d79d3b7a749cc8a
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy form/DayOfWeekSelector.js from manager to manager5

---
+++ www/manager5/form/DayOfWeekSelector.js
@@ -0,0 +1,20 @@
+Ext.define('PVE.form.DayOfWeekSelector', {
+    extend: 'PVE.form.KVComboBox',
+    alias: ['widget.pveDayOfWeekSelector'],
+  
+    initComponent: function() {
+	var me = this;
+
+	me.data = [
+	    ['sun', Ext.util.Format.htmlDecode(Ext.Date.dayNames[0])],
+	    ['mon', Ext.util.Format.htmlDecode(Ext.Date.dayNames[1])],
+	    ['tue', Ext.util.Format.htmlDecode(Ext.Date.dayNames[2])],
+	    ['wed', Ext.util.Format.htmlDecode(Ext.Date.dayNames[3])],
+	    ['thu', Ext.util.Format.htmlDecode(Ext.Date.dayNames[4])],
+	    ['fri', Ext.util.Format.htmlDecode(Ext.Date.dayNames[5])],
+	    ['sat', Ext.util.Format.htmlDecode(Ext.Date.dayNames[6])]
+	];
+
+	me.callParent();
+    }
+});
