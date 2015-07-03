ea0d76d688eed7c06f7b506413c9e1696b2fa5f9
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy window/NotesEdit.js from manager to manager5

---
+++ www/manager5/window/NotesEdit.js
@@ -0,0 +1,24 @@
+Ext.define('PVE.window.NotesEdit', {
+    extend: 'PVE.window.Edit',
+
+    initComponent : function() {
+	var me = this;
+
+	Ext.apply(me, {
+	    title: gettext('Notes'),
+	    width: 600,
+	    layout: 'fit',
+	    items: {
+		xtype: 'textarea',
+		name: 'description',
+		rows: 7,
+		value: '',
+		hideLabel: true
+	    }
+	});
+
+	me.callParent();
+
+	me.load();
+    }
+});
