b1821bc1cd9402461a22f537e92dba99d0f25454
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy form/CPUModelSelector.js from manager to manager5

---
+++ www/manager5/form/CPUModelSelector.js
@@ -0,0 +1,40 @@
+Ext.define('PVE.form.CPUModelSelector', {
+    extend: 'PVE.form.KVComboBox',
+    alias: ['widget.CPUModelSelector'],
+  
+    initComponent: function() {
+	var me = this;
+
+        me.data = [ 
+	    ['', PVE.Utils.defaultText + ' (kvm64)'],
+	    ['486', '486'],
+	    ['athlon', 'athlon'],
+	    ['core2duo', 'core2duo'],
+	    ['coreduo', 'coreduo'],
+	    ['kvm32', 'kvm32'],
+	    ['kvm64', 'kvm64'],
+	    ['pentium', 'pentium'],
+	    ['pentium2', 'pentium2'],
+	    ['pentium3', 'pentium3'],
+	    ['phenom', 'phenom'],
+	    ['qemu32', 'qemu32'],
+	    ['qemu64', 'qemu64'],
+	    ['Conroe', 'Conroe'],
+	    ['Penryn', 'Penryn'],
+	    ['Nehalem', 'Nehalem'],
+	    ['Westmere', 'Westmere'],
+	    ['SandyBridge', 'SandyBridge'],
+	    ['IvyBridge', 'IvyBridge'],
+	    ['Haswell', 'Haswell'],
+	    ['Broadwell', 'Broadwell'],
+	    ['Opteron_G1', 'Opteron_G1'],
+	    ['Opteron_G2', 'Opteron_G2'],
+	    ['Opteron_G3', 'Opteron_G3'],
+	    ['Opteron_G4', 'Opteron_G4'],
+	    ['Opteron_G5', 'Opteron_G5'],
+	    ['host', 'host']
+	];
+
+	me.callParent();
+    }
+});
