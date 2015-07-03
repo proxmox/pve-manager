c801943bb56dbee878e7387d41d081172fea38b7
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy pool/StatusView.js from manager to manager5

---
+++ www/manager5/pool/StatusView.js
@@ -0,0 +1,31 @@
+Ext.define('PVE.pool.StatusView', {
+    extend: 'PVE.grid.ObjectGrid',
+    alias: ['widget.pvePoolStatusView'],
+
+    initComponent : function() {
+	var me = this;
+
+	var pool = me.pveSelNode.data.pool;
+	if (!pool) {
+	    throw "no pool specified";
+	}
+
+	var rows = {
+	    comment: {
+		header: gettext('Comment'), 
+		required: true
+	    }
+	};
+
+	Ext.applyIf(me, {
+	    title: gettext('Status'),
+	    url: "/api2/json/pools/" + pool,
+	    cwidth1: 150,
+	    interval: 30000,
+	    //height: 195,
+	    rows: rows
+	});
+
+	me.callParent();
+    }
+});
