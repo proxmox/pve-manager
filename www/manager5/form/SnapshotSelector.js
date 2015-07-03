178a97edbcac4e9550855e9292cb4f05b97840a3
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy form/SnapshotSelector.js from manager to manager5

---
+++ www/manager5/form/SnapshotSelector.js
@@ -0,0 +1,64 @@
+Ext.define('PVE.form.SnapshotSelector', {
+    extend: 'PVE.form.ComboGrid',
+    alias: ['widget.PVE.form.SnapshotSelector'],
+
+    loadStore: function(nodename, vmid) {
+	var me = this;
+
+	if (!nodename) {
+	    return;
+	}
+
+	me.nodename = nodename;
+
+        if (!vmid) {
+	    return;
+        }
+
+	me.vmid = vmid;
+
+	me.store.setProxy({
+	    type: 'pve',
+	    url: '/api2/json/nodes/' + me.nodename + '/qemu/' + me.vmid +'/snapshot'
+	});
+
+	me.store.load();
+    },
+
+    initComponent: function() {
+	var me = this;
+
+        if (!me.nodename) {
+            throw "no node name specified";
+        }
+
+        if (!me.vmid) {
+            throw "no VM ID specified";
+        }
+
+	var store = Ext.create('Ext.data.Store', {
+	    fields: [ 'name'],
+	    filterOnLoad: true
+	});
+
+	Ext.apply(me, {
+	    store: store,
+	    valueField: 'name',
+	    displayField: 'name',
+            listConfig: {
+		columns: [
+		    {
+			header: gettext('Snapshot'),
+			dataIndex: 'name',
+			hideable: false,
+			flex: 1
+		    }
+		]
+	    }
+	});
+
+        me.callParent();
+
+	me.loadStore(me.nodename, me.vmid);
+    }
+});
