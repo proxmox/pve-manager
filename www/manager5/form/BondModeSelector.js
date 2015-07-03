056115579a6e867e652183bc814ed985603bed9b
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy form/BondModeSelector.js from manager to manager5

---
+++ www/manager5/form/BondModeSelector.js
@@ -0,0 +1,48 @@
+Ext.define('PVE.form.BondModeSelector', {
+    extend: 'PVE.form.KVComboBox',
+    alias: ['widget.bondModeSelector'],
+  
+    openvswitch: false,
+
+    initComponent: function() {
+	var me = this;
+
+	if (me.openvswitch) {
+           me.data = [ 
+	       ['active-backup', 'active-backup'],
+	       ['balance-slb', 'balance-slb'],
+	       ['lacp-balance-slb', 'LACP (balance-slb)'],
+	       ['lacp-balance-tcp', 'LACP (balance-tcp)']
+	   ];
+	} else {
+            me.data = [ 
+		['balance-rr', 'balance-rr'], 
+		['active-backup', 'active-backup'], 
+		['balance-xor', 'balance-xor'], 
+		['broadcast', 'broadcast'], 
+		['802.3ad', 'LACP (802.3ad)'], 
+		['balance-tlb', 'balance-tlb'], 
+		['balance-alb', 'balance-alb']
+	    ];
+	}
+ 
+	me.callParent();
+    }
+});
+
+Ext.define('PVE.form.BondPolicySelector', {
+    extend: 'PVE.form.KVComboBox',
+    alias: ['widget.bondPolicySelector'],
+
+    initComponent: function() {
+	var me = this;
+        me.data = [
+	    ['layer2', 'layer2'],
+	    ['layer2+3', 'layer2+3'], 
+	    ['layer3+4', 'layer3+4']
+	];
+ 
+	me.callParent();
+    }
+});
+
