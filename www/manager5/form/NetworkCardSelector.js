b9698f6438aa04c161cb58c1193422bfdb7c9a16
Dietmar Maurer <dietmar@proxmox.com>
2015-07-03 11:29:11 +0200

    copy form/NetworkCardSelector.js from manager to manager5

---
+++ www/manager5/form/NetworkCardSelector.js
@@ -0,0 +1,17 @@
+Ext.define('PVE.form.NetworkCardSelector', {
+    extend: 'PVE.form.KVComboBox',
+    alias: ['widget.PVE.form.NetworkCardSelector'],
+  
+    initComponent: function() {
+	var me = this;
+
+        me.data = [ 
+	    ['e1000', 'Intel E1000'],
+	    ['virtio', 'VirtIO (' + gettext('paravirtualized') + ')'],
+	    ['rtl8139', 'Realtec RTL8139'],
+	    ['vmxnet3', 'VMWare vmxnet3']
+	];
+ 
+	me.callParent();
+    }
+});
