ab4ae3dcfdee8b768af1bca54eef07eb4c448570
Dietmar Maurer <dietmar@proxmox.com>
2015-09-07 10:08:08 +0200

    convert pvedaemon into a PVE::Service class

---
+++ PVE/Service/pvedaemon.pm
@@ -0,0 +1,84 @@
+package PVE::Service::pvedaemon;
+
+use strict;
+use warnings;
+
+use PVE::SafeSyslog;
+use PVE::Daemon;
+use PVE::API2;
+use PVE::API2::Formatter::Standard;
+use PVE::API2::Formatter::HTML;
+use PVE::HTTPServer;
+
+use base qw(PVE::Daemon);
+
+my $cmdline = [$0, @ARGV];
+
+my %daemon_options = (
+    max_workers => 3,
+    restart_on_error => 5,
+    stop_wait_time => 15,
+    leave_children_open_on_reload => 1,
+);
+
+my $daemon = __PACKAGE__->new('pvedaemon', $cmdline, %daemon_options);
+
+sub init {
+    my ($self) = @_;
+
+    my $accept_lock_fn = "/var/lock/pvedaemon.lck";
+
+    my $lockfh = IO::File->new(">>${accept_lock_fn}") ||
+	die "unable to open lock file '${accept_lock_fn}' - $!\n";
+
+    my $socket = $self->create_reusable_socket(85, '127.0.0.1');
+
+    $self->{server_config} = {
+	base_handler_class => 'PVE::API2',
+	keep_alive => 100,
+	max_conn => 500,
+	max_requests => 1000,
+	lockfile => $accept_lock_fn,
+	socket => $socket,
+	lockfh => $lockfh,
+	debug => $self->{debug},
+	trusted_env => 1,
+    };
+}
+
+sub run {
+    my ($self) = @_;
+
+    my $server = PVE::HTTPServer->new(%{$self->{server_config}});
+    $server->run();
+}
+
+$daemon->register_start_command();
+$daemon->register_restart_command(1);
+$daemon->register_stop_command();
+$daemon->register_status_command();
+
+our $cmddef = {
+    start => [ __PACKAGE__, 'start', []],
+    restart => [ __PACKAGE__, 'restart', []],
+    stop => [ __PACKAGE__, 'stop', []],
+    status => [ __PACKAGE__, 'status', [], undef, sub { print shift . "\n";} ],
+};
+
+1;
+
+__END__
+
+=head1 NAME
+
+pvedaemon - the PVE configuration server
+
+=include synopsis
+
+=head1 DESCRIPTION
+
+All configuration is done using this Server. The Server only
+listens to a local address 127.0.0.1 port 85 for security
+reasons.
+
+=include pve_copyright
