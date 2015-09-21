1daa93803f30601f786b5e223195c5546efa18d4
Dietmar Maurer <dietmar@proxmox.com>
2015-09-21 09:50:34 +0200

    Revert "fix bug #706: Use comma to split vmid list when building the crontab vzdump command"
    This reverts commit 4bf3cc21b676157897764246e0da1cb96582e796.
    
    No longer required, because vzdump accepts space separated vmid lists again.

--- PVE/VZDump.pm
+++ PVE/VZDump.pm
@@ -1266,7 +1266,7 @@ sub command_line {
     my $cmd = "vzdump";
 
     if ($param->{vmid}) {
-	$cmd .= " " . join(',', PVE::Tools::split_list($param->{vmid}));
+	$cmd .= " " . join(' ', PVE::Tools::split_list($param->{vmid}));
     }
 
     foreach my $p (keys %$param) {
