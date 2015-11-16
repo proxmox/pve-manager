04ebb5c650280b5b3d3217568d9b6d08861c9262
Emmanuel Kasper <e.kasper@proxmox.com>
2015-11-16 14:02:16 +0100

    ext6migrate: move class static properties out of initComponent()
    This fixes the Auth realm selection while login with ExtJS6

--- www/manager6/form/RealmComboBox.js
+++ www/manager6/form/RealmComboBox.js
@@ -2,6 +2,28 @@ Ext.define('PVE.form.RealmComboBox', {
     extend: 'Ext.form.field.ComboBox',
     alias: ['widget.pveRealmComboBox'],
 
+    fieldLabel: gettext('Realm'),
+    name: 'realm',
+    queryMode: 'local',
+    allowBlank: false,
+    forceSelection: true,
+    autoSelect: false,
+    triggerAction: 'all',
+    valueField: 'realm',
+    displayField: 'descr',
+    getState: function() {
+	return { value: this.getValue() };
+    },
+    applyState : function(state) {
+	if (state && state.value) {
+	    this.setValue(state.value);
+	}
+    },
+    stateEvents: [ 'select' ],
+    stateful: true,
+    id: 'pveloginrealm', // fixme: remove (Stateful does not work without)
+    stateID: 'pveloginrealm',
+
     needOTP: function(realm) {
 	var me = this;
 
@@ -13,44 +35,17 @@ Ext.define('PVE.form.RealmComboBox', {
     initComponent: function() {
 	var me = this;
 
-	var stateid = 'pveloginrealm';
-
-	var realmstore = Ext.create('Ext.data.Store', {
+	me.store = Ext.create('Ext.data.Store', {
 	    model: 'pve-domains',
-	});
-
-	Ext.apply(me, {
-	    fieldLabel: gettext('Realm'),
-	    name: 'realm',
-	    store: realmstore,
-	    queryMode: 'local',
-	    allowBlank: false,
-	    forceSelection: true,
-	    autoSelect: false,
-	    triggerAction: 'all',
-	    valueField: 'realm',
-	    displayField: 'descr',
-	    getState: function() {
-		return { value: this.getValue() };
-	    },
-	    applyState : function(state) {
-		if (state && state.value) {
-		    this.setValue(state.value);
-		}
-	    },
-	    stateEvents: [ 'select' ],
-	    stateful: true,
-	    id: stateid, // fixme: remove (Stateful does not work without)  
-	    stateID: stateid
 	});
 
         me.callParent();
 
-	realmstore.load({
+	me.store.load({
 	    callback: function(r, o, success) {
 		if (success) {
 		    var def = me.getValue();
-		    if (!def || !realmstore.findRecord('realm', def)) {
+		    if (!def || !me.store.findRecord('realm', def)) {
 			def = 'pam';
 			Ext.each(r, function(record) {
 			    if (record.data && record.data["default"]) { 
