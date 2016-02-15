bc0c438d171b9327918b877e22a5ea81fef7f1ef
Dietmar Maurer <dietmar@proxmox.com>
2016-02-15 12:35:13 +0100

    copy ha/Fencing.js from manager to manager6

---
+++ www/manager6/ha/Fencing.js
@@ -0,0 +1,47 @@
+Ext.define('PVE.ha.FencingView', {
+    extend: 'Ext.grid.GridPanel',
+    alias: ['widget.pveFencingView'],
+
+    initComponent : function() {
+	var me = this;
+
+	var store = new Ext.data.Store({
+	    model: 'pve-ha-fencing',
+	    data: []
+	});
+
+	Ext.apply(me, {
+	    store: store,
+	    stateful: false,
+	    viewConfig: {
+		trackOver: false,
+		deferEmptyText: false,
+		emptyText: 'Use watchdog based fencing.'
+	    },
+	    columns: [
+		{
+		    header: 'Node',
+		    width: 100,
+		    sortable: true,
+		    dataIndex: 'node'
+		},
+		{
+		    header: gettext('Command'),
+		    flex: 1,
+		    dataIndex: 'command'
+		}
+	    ]
+	});
+
+	me.callParent();
+    }
+}, function() {
+
+    Ext.define('pve-ha-fencing', {
+	extend: 'Ext.data.Model',
+	fields: [ 
+	    'node', 'command', 'digest'
+	]
+    });
+
+});
