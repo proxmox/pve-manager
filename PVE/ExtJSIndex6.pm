36fd2767a6e2c42613bff8bc245af727f1af162e
Emmanuel Kasper <e.kasper@proxmox.com>
2016-02-15 13:29:58 +0100

    Fix warning in Browser console due to missing meta entry
    We're not going to support mobile devices via the standard gui,
    but ExtJS 6 themes need this

--- PVE/ExtJSIndex6.pm
+++ PVE/ExtJSIndex6.pm
@@ -21,6 +21,7 @@ sub get_index {
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>$nodename - Proxmox Virtual Environment</title>
     <link rel="stylesheet" type="text/css" href="/pve2/ext6/theme-triton/resources/theme-triton-all.css" />
     <link rel="stylesheet" type="text/css" href="/pve2/css/ext-pve.css" />
