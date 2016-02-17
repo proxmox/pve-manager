f48586ba8621b172cf2ee473b8d1695d748362a8
Dietmar Maurer <dietmar@proxmox.com>
2016-02-17 11:55:32 +0100

    copy node/TimeEdit.js from manager to manager6

---
+++ www/manager6/node/TimeEdit.js
@@ -0,0 +1,39 @@
+Ext.define('PVE.node.TimeEdit', {
+    extend: 'PVE.window.Edit',
+    alias: ['widget.pveNodeTimeEdit'],
+
+    initComponent : function() {
+	var me = this;
+
+	var nodename = me.pveSelNode.data.node;
+	if (!nodename) {
+	    throw "no node name specified";
+	}
+
+	Ext.applyIf(me, {
+	    subject: gettext('Time zone'),
+	    url: "/api2/extjs/nodes/" + nodename + "/time",
+	    fieldDefaults: {
+		labelWidth: 70
+            },
+	    width: 400,
+	    items: {
+		xtype: 'combo',
+		fieldLabel: gettext('Time zone'),
+		name: 'timezone',
+		queryMode: 'local',
+		store: new PVE.data.TimezoneStore(),
+		valueField: 'zone',
+		displayField: 'zone',
+		triggerAction: 'all',
+		forceSelection: true,
+		editable: false,
+		allowBlank: false
+	    }
+	});
+
+	me.callParent();
+
+	me.load();
+    }
+});
