0568e342e83b2c0d400d6712138adc66354ffa0b
Jean-Francois Dagenais <jeff.dagenais@gmail.com>
2016-03-03 09:48:40 -0500

    vzdump: make sure only DOT pid files excluded
    and not files like "acpid"
    
    Signed-off-by: Jean-Francois Dagenais <jeff.dagenais@gmail.com>

--- PVE/VZDump.pm
+++ PVE/VZDump.pm
@@ -593,7 +593,7 @@ sub new {
 	push @$findexcl, '/var/log/?*',
 	                 '/tmp/?*',
 	                 '/var/tmp/?*',
-	                 '/var/run/?*pid';
+	                 '/var/run/?*.pid';
     }
 
     foreach my $p (@plugins) {
