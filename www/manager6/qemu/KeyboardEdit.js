bf8873e89dc712147be582866803dc0e91ab1a61
Dietmar Maurer <dietmar@proxmox.com>
2016-03-16 09:12:22 +0100

    copy qemu/KeyboardEdit.js from manager to manager6

---
+++ www/manager6/qemu/KeyboardEdit.js
@@ -0,0 +1,21 @@
+Ext.define('PVE.qemu.KeyboardEdit', {
+    extend: 'PVE.window.Edit',
+
+    initComponent : function() {
+	var me = this;
+
+	Ext.applyIf(me, {
+	    subject: gettext('Keyboard Layout'),
+	    items: {
+		xtype: 'VNCKeyboardSelector',
+		name: 'keyboard',
+		value: '',
+		fieldLabel: gettext('Keyboard Layout')
+	    }
+	});
+
+	me.callParent();
+
+	me.load();
+    }
+});
