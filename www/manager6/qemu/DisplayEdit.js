db0501804f7024b95118fe4e224bfbf86e24ccfa
Dietmar Maurer <dietmar@proxmox.com>
2016-03-16 09:12:22 +0100

    copy qemu/DisplayEdit.js from manager to manager6

---
+++ www/manager6/qemu/DisplayEdit.js
@@ -0,0 +1,50 @@
+Ext.define('PVE.qemu.DisplayEdit', {
+    extend: 'PVE.window.Edit',
+
+    vmconfig: undefined,
+
+    initComponent : function() {
+	var me = this;
+
+	var displayField;
+
+	var validateDisplay = function() {
+	    /*jslint confusion: true */
+	    var val = displayField.getValue();
+
+	    if (me.vmconfig && val.match(/^serial\d+$/)) {
+		if (me.vmconfig[val] && me.vmconfig[val] === 'socket') {
+		    return true;
+		}
+		return "Serial interface '" + val + "' is not correctly configured.";
+	    }
+	    
+	    return true;
+	};
+
+	displayField = Ext.createWidget('DisplaySelector', {  
+	    name: 'vga',
+	    value: '',
+	    fieldLabel: gettext('Graphic card'),
+	    validator: validateDisplay
+	});
+
+	Ext.apply(me, {
+	    subject: gettext('Display'),
+	    width: 350,
+	    items: displayField
+	});
+
+	me.callParent();
+
+	me.load({
+	    success: function(response, options) {
+		var values = response.result.data;
+
+		me.vmconfig = values;
+
+		me.setValues(values);
+	    }
+	});
+    }
+});
