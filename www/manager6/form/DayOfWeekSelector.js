c0b9cfab9653d26e7447024d9ffde4724db898e8
Dominik Csapak <d.csapak@proxmox.com>
2016-04-06 14:34:56 +0200

    add weekdays to comboitems at runtime
    the translation of extjs has not been applied
    at the time of class declaration, so that
    the weekdays were always "Sunday", "Monday", etc.
    
    when we declare them in initComponent, the translations are there
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/manager6/form/DayOfWeekSelector.js
+++ www/manager6/form/DayOfWeekSelector.js
@@ -1,7 +1,10 @@
 Ext.define('PVE.form.DayOfWeekSelector', {
     extend: 'PVE.form.KVComboBox',
     alias: ['widget.pveDayOfWeekSelector'],
-    comboItems: [
+    comboItems:[],
+    initComponent: function(){
+	var me = this;
+	me.comboItems = [
 	    ['sun', Ext.util.Format.htmlDecode(Ext.Date.dayNames[0])],
 	    ['mon', Ext.util.Format.htmlDecode(Ext.Date.dayNames[1])],
 	    ['tue', Ext.util.Format.htmlDecode(Ext.Date.dayNames[2])],
@@ -9,5 +12,7 @@ Ext.define('PVE.form.DayOfWeekSelector', {
 	    ['thu', Ext.util.Format.htmlDecode(Ext.Date.dayNames[4])],
 	    ['fri', Ext.util.Format.htmlDecode(Ext.Date.dayNames[5])],
 	    ['sat', Ext.util.Format.htmlDecode(Ext.Date.dayNames[6])]
-	]
+	];
+	this.callParent();
+    }
 });
