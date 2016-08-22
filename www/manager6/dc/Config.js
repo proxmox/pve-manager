2af89b7b595aa36b095b1546d3c711fcaf5b2676
Dominik Csapak <d.csapak@proxmox.com>
2016-08-22 17:13:34 +0200

    reorganize datacenter items
    use the new tree functionality of the configpanel
    make users,groups,pools,roles and auth
    subitems of permissions (expanded by default)
    
    instead of using the subconfigpanel for
    ha and firewall, use the panels directly
    as subitems of ha-status and firewall-rules
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/manager6/dc/Config.js
+++ www/manager6/dc/Config.js
@@ -22,89 +22,154 @@ Ext.define('PVE.dc.Config', {
 	    me.items.push({
 	    title: gettext('Summary'),
 		xtype: 'pveDcSummary',
+		iconCls: 'fa fa-book',
 		itemId: 'summary'
-		});
+	    });
 
 	    me.items.push({
 	    xtype: 'pveDcOptionView',
 		title: gettext('Options'),
+		iconCls: 'fa fa-gear',
 		itemId: 'options'
-		});
+	    });
 	}
 
 	if (caps.storage['Datastore.Allocate'] || caps.dc['Sys.Audit']) {
 	    me.items.push({
 		xtype: 'pveStorageView',
 		title: gettext('Storage'),
+		iconCls: 'fa fa-database',
 		itemId: 'storage'
 	    });
 	}
 
 	if (caps.dc['Sys.Audit']) {
-		me.items.push({
+	    me.items.push({
 		xtype: 'pveDcBackupView',
+		iconCls: 'fa fa-floppy-o',
 		title: gettext('Backup'),
 		itemId: 'backup'
+	    },
+	    {
+		xtype: 'pveACLView',
+		title: gettext('Permissions'),
+		iconCls: 'fa fa-unlock',
+		itemId: 'permissions',
+		expandedOnInit: true
 	    });
 	}
 
 	me.items.push({
 	    xtype: 'pveUserView',
+	    groups: ['permissions'],
+	    iconCls: 'fa fa-user',
 	    title: gettext('Users'),
 	    itemId: 'users'
 	});
 
 	if (caps.dc['Sys.Audit']) {
 	    me.items.push({
-		    xtype: 'pveGroupView',
-		    title: gettext('Groups'),
-		    itemId: 'groups'
-		});
-
-	    me.items.push({
-		    xtype: 'pvePoolView',
-		    title: gettext('Pools'),
-		    itemId: 'pools'
-		});
-
-	    me.items.push({
-		    xtype: 'pveACLView',
-		    title: gettext('Permissions'),
-		    itemId: 'permissions'
-		});
-
-	    me.items.push({
-		    xtype: 'pveRoleView',
-		    title: gettext('Roles'),
-		    itemId: 'roles'
-		});
-
-	    me.items.push({
-		    xtype: 'pveAuthView',
-		    title: gettext('Authentication'),
-		    itemId: 'domains'
-		});
-
-	    me.items.push({
-		    xtype: 'pveHAPanel',
-		    title: 'HA',
-		    phstateid: me.hstateid,
-		    itemId: 'ha'
-		});
-
-	    me.items.push({
-		    xtype: 'pveFirewallPanel',
-		    title: gettext('Firewall'),
-		    base_url: '/cluster/firewall',
-		    fwtype: 'dc',
-		    phstateid: me.hstateid,
-		    itemId: 'firewall'
-		});
-
-	    me.items.push({
+		xtype: 'pveGroupView',
+		title: gettext('Groups'),
+		iconCls: 'fa fa-users',
+		groups: ['permissions'],
+		itemId: 'groups'
+	    },
+	    {
+		xtype: 'pvePoolView',
+		title: gettext('Pools'),
+		iconCls: 'fa fa-tags',
+		groups: ['permissions'],
+		itemId: 'pools'
+	    },
+	    {
+		xtype: 'pveRoleView',
+		title: gettext('Roles'),
+		iconCls: 'fa fa-male',
+		groups: ['permissions'],
+		itemId: 'roles'
+	    },
+	    {
+		xtype: 'pveAuthView',
+		title: gettext('Authentication'),
+		groups: ['permissions'],
+		iconCls: 'fa fa-key',
+		itemId: 'domains'
+	    },
+	    {
+		xtype: 'pveHAStatusView',
+		title: 'HA',
+		iconCls: 'fa fa-heartbeat',
+		itemId: 'ha'
+	    },
+	    {
+		title: gettext('Resources'),
+		groups: ['ha'],
+		iconCls: 'fa fa-th',
+		xtype: 'pveHAResourcesView',
+		itemId: 'resources'
+	    },
+	    {
+		title: gettext('Groups'),
+		groups: ['ha'],
+		xtype: 'pveHAGroupsView',
+		iconCls: 'fa fa-object-group',
+		itemId: 'ha-groups'
+	    },
+	    {
+		title: gettext('Fencing'),
+		groups: ['ha'],
+		iconCls: 'fa fa-bolt',
+		xtype: 'pveFencingView',
+		itemId: 'ha-fencing'
+	    },
+	    {
+		xtype: 'pveFirewallRules',
+		title: gettext('Firewall'),
+		allow_iface: true,
+		base_url: '/cluster/firewall/rules',
+		list_refs_url: '/cluster/firewall/refs',
+		iconCls: 'fa fa-shield',
+		itemId: 'firewall'
+	    },
+	    {
+		xtype: 'pveFirewallOptions',
+		title: gettext('Options'),
+		groups: ['firewall'],
+		iconCls: 'fa fa-gear',
+		base_url: '/cluster/firewall/options',
+		fwtype: 'dc',
+		itemId: 'firewall-options'
+	    },
+	    {
+		xtype: 'pveSecurityGroups',
+		title: gettext('Security Group'),
+		groups: ['firewall'],
+		iconCls: 'fa fa-group',
+		itemId: 'firewall-sg'
+	    },
+	    {
+		xtype: 'pveFirewallAliases',
+		title: gettext('Alias'),
+		groups: ['firewall'],
+		iconCls: 'fa fa-external-link',
+		base_url: '/cluster/firewall/aliases',
+		itemId: 'firewall-aliases'
+	    },
+	    {
+		xtype: 'pveIPSet',
+		title: 'IPSet',
+		groups: ['firewall'],
+		iconCls: 'fa fa-list-ol',
+		base_url: '/cluster/firewall/ipset',
+		list_refs_url: '/cluster/firewall/refs',
+		itemId: 'firewall-ipset'
+	    },
+	    {
 		xtype: 'pveDcSupport',
 		title: gettext('Support'),
-		itemId: 'support'
+		itemId: 'support',
+		iconCls: 'fa fa-comments-o'
 	    });
 	}
 
