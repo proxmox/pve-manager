26dccfdb4922c825bb338cda14ab5a735fa9053c
Dietmar Maurer <dietmar@proxmox.com>
2016-12-28 12:06:27 +0100

    perftest2.pl: use -k (keepalive) to show max performance

--- bin/test/perftest2.pl
+++ bin/test/perftest2.pl
@@ -13,6 +13,6 @@ my $hostname = PVE::INotify::read_file("hostname");
 # but we can simply create a ticket if we are root
 my $ticket = PVE::AccessControl::assemble_ticket('root@pam');
 
-my $cmd = "ab -c 10 -n 1000 -C 'PVEAuthCookie=$ticket'  https://$hostname:8006/api2/json";
+my $cmd = "ab -c 10 -n 1000 -k -C 'PVEAuthCookie=$ticket'  https://$hostname:8006/api2/json";
 print "$cmd\n";
 system($cmd) == 0 || die "command failed - $!\n";
