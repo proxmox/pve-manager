abbc1b79480f3301dd0217ab90f0a4e974b57b30
Dietmar Maurer <dietmar@proxmox.com>
2017-06-29 07:58:37 +0200

    Revert "add beta text with link to bugtracker"
    This reverts commit 6531861e690bbe2c077dd0388689e565cf28df5a.
    
    Prepare for final 5.0 release.

--- www/manager6/Workspace.js
+++ www/manager6/Workspace.js
@@ -302,10 +302,6 @@ Ext.define('PVE.StdWorkspace', {
 			    html: 'Virtual Environment'
 			},
 			{
-			    padding: 5,
-			    html: '<a href="https://bugzilla.proxmox.com" target="_blank">BETA</a>'
-			},
-			{
 			    xtype: 'pveGlobalSearchField',
 			    tree: rtree
 			},
