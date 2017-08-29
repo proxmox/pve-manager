3dc5644c9ec690f3f6d14fac543cc8c568b25256
Dominik Csapak <d.csapak@proxmox.com>
2017-08-29 11:41:18 +0200

    hide migrate button in lxc for non cluster setups
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/lxc/Config.js
+++ www/manager6/lxc/Config.js
@@ -85,6 +85,7 @@ Ext.define('PVE.lxc.Config', {
 	var migrateBtn = Ext.create('Ext.Button', {
 	    text: gettext('Migrate'),
 	    disabled: !caps.vms['VM.Migrate'],
+	    hidden: PVE.data.ResourceStore.getNodes().length < 2,
 	    handler: function() {
 		var win = Ext.create('PVE.window.Migrate', {
 		    vmtype: 'lxc',
