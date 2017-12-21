proxmox/pve-manager

commit 551456ff6ef7d3591178978fbd3dc760e35d7874

add defaultButton to loginWindow
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

www/manager6/window/LoginWindow.js
@@ -129,6 +129,8 @@ Ext.define('PVE.window.LoginWindow', {
 
     defaultFocus: 'usernameField',
 
+    defaultButton: 'loginButton',
+
     items: [{
 	xtype: 'form',
 	layout: 'form',

commit 26031ab15f0f66a366a74a9130451ee3fedfb8f8

prevent double ticket api call
commit:
551456ff6ef7d3591178978fbd3dc760e35d7874

added the defaultButton property to the loginwindow, but we missed that
pressing enter in the passwordfield/userfield led to a double ticket
api call.

while it did not any harm, remove it, so that we only have a single
ticket api call

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Acked-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
Fixes: 551456ff6ef7 ("add defaultButton to loginWindow")

www/manager6/window/LoginWindow.js
@@ -53,18 +53,9 @@ Ext.define('PVE.window.LoginWindow', {
 		specialkey: function(f, e) {
 		    if (e.getKey() === e.ENTER) {
 			var pf = this.lookupReference('passwordField');
-			if (pf.getValue()) {
-			    this.onLogon();
-			} else {
+			if (!pf.getValue()) {
 			    pf.focus(false);
 			}
-		    }
-		}
-	    },
-	    'field[name=password]': {
-		specialkey: function(f, e) {
-		    if (e.getKey() === e.ENTER) {
-			this.onLogon();
 		    }
 		}
 	    },