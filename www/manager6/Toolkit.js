f6673991d5e904063d3a46ebab7f0e39ca9d055b
Dominik Csapak <d.csapak@proxmox.com>
2018-03-15 16:21:30 +0100

    add IP64AddressList vtype
    this can be used for DNS Server list
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/manager6/Toolkit.js
+++ www/manager6/Toolkit.js
@@ -8,6 +8,22 @@ Ext.apply(Ext.form.field.VTypes, {
     QemuStartDate: function(v) {
 	return (/^(now|\d{4}-\d{1,2}-\d{1,2}(T\d{1,2}:\d{1,2}:\d{1,2})?)$/).test(v);
     },
-    QemuStartDateText: gettext('Format') + ': "now" or "2006-06-17T16:01:21" or "2006-06-17"'
+    QemuStartDateText: gettext('Format') + ': "now" or "2006-06-17T16:01:21" or "2006-06-17"',
+    IP64AddressList: function(v) {
+	var list = v.split(/[\ \,\;]+/);
+	var i;
+	for (i = 0; i < list.length; i++) {
+	    if (list[i] == '') {
+		continue;
+	    }
 
+	    if (!Proxmox.Utils.IP64_match.test(list[i])) {
+		return false;
+	    }
+	}
+
+	return true;
+    },
+    IP64AddressListText: gettext('Example') + ': 192.168.1.1,192.168.1.2',
+    IP64AddressListMask: /[A-Fa-f0-9\,\:\.\;\ ]/
 });
