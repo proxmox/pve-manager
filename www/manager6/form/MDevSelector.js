02cebb1a46de74a8659d370447da222d50c116b7
Dominik Csapak <d.csapak@proxmox.com>
2018-11-22 11:35:44 +0100

    gui: add MDevSelector
    this is used to select a mediated device type
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ www/manager6/form/MDevSelector.js
@@ -0,0 +1,91 @@
+Ext.define('PVE.form.MDevSelector', {
+    extend: 'Proxmox.form.ComboGrid',
+    xtype: 'pveMDevSelector',
+
+    store: {
+	fields: [ 'type','available', 'description' ],
+	filterOnLoad: true,
+	sorters: [
+	    {
+		property : 'type',
+		direction: 'ASC'
+	    }
+	]
+    },
+    autoSelect: false,
+    valueField: 'type',
+    displayField: 'type',
+    listConfig: {
+	columns: [
+	    {
+		header: gettext('Type'),
+		dataIndex: 'type',
+		flex: 1
+	    },
+	    {
+		header: gettext('Available'),
+		dataIndex: 'available',
+		width: 80
+	    },
+	    {
+		header: gettext('Description'),
+		dataIndex: 'description',
+		flex: 1,
+		renderer: function(value) {
+		    if (!value) {
+			return '';
+		    }
+
+		    return value.split('\n').join('<br>');
+		}
+	    }
+	]
+    },
+
+    setPciID: function(pciid, force) {
+	var me = this;
+
+	if (!force && (!pciid || (me.pciid === pciid))) {
+	    return;
+	}
+
+	me.pciid = pciid;
+	me.updateProxy();
+    },
+
+
+    setNodename: function(nodename) {
+	var me = this;
+
+	if (!nodename || (me.nodename === nodename)) {
+	    return;
+	}
+
+	me.nodename = nodename;
+	me.updateProxy();
+    },
+
+    updateProxy: function() {
+	var me = this;
+	me.store.setProxy({
+	    type: 'proxmox',
+	    url: '/api2/json/nodes/' + me.nodename + '/scan/pci/' + me.pciid + '/mdev'
+	});
+	me.store.load();
+    },
+
+    initComponent: function() {
+	var me = this;
+
+	if (!me.nodename) {
+	    throw 'no node name specified';
+	}
+
+        me.callParent();
+
+	if (me.pciid) {
+	    me.setPciID(me.pciid, true);
+	}
+    }
+});
+
