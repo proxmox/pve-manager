3f2300d8b4e0a0560c00be5c3e31b8e851dfeadb
Thomas Lamprecht <t.lamprecht@proxmox.com>
2018-11-22 15:05:52 +0100

    api/hardware: fixup: add missing file
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ PVE/API2/Hardware.pm
@@ -0,0 +1,51 @@
+package PVE::API2::Hardware;
+
+use strict;
+use warnings;
+
+use PVE::JSONSchema qw(get_standard_option);
+use PVE::RESTHandler;
+
+use PVE::API2::Hardware::PCI;
+
+use base qw(PVE::RESTHandler);
+
+__PACKAGE__->register_method ({
+    subclass => "PVE::API2::Hardware::PCI",
+    path => 'pci',
+});
+
+__PACKAGE__->register_method ({
+    name => 'index',
+    path => '',
+    method => 'GET',
+    description => "Index of hardware types",
+    permissions => {
+	user => 'all',
+    },
+    parameters => {
+	additionalProperties => 0,
+	properties => {
+	    node => get_standard_option('pve-node'),
+	},
+    },
+    returns => {
+	type => 'array',
+	items => {
+	    type => "object",
+	    properties => { type => { type => 'string'} },
+	},
+	links => [ { rel => 'child', href => "{type}" } ],
+    },
+    code => sub {
+	my ($param) = @_;
+
+	my $res = [
+	    { type => 'pci' },
+	    # TODO: move usb scan here (6.0 api change)
+	];
+
+	return $res;
+    }
+});
+
