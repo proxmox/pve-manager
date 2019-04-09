1b364b3a98f6c8decead13cc6ff62fedf4ef3ba3
Alwin Antreich <a.antreich@proxmox.com>
2019-04-09 10:44:43 +0200

    pveceph: install ceph service print sucess message
    This patch adds a success message on successful ceph.service
    installation. And adds a newline to make a successful ceph package
    installation more visible.
    
    Signed-off-by: Alwin Antreich <a.antreich@proxmox.com>

--- PVE/CLI/pveceph.pm
+++ PVE/CLI/pveceph.pm
@@ -151,13 +151,18 @@ __PACKAGE__->register_method ({
 
 	if (PVE::Ceph::Tools::systemd_managed() && ! -e '/etc/systemd/system/ceph.service') {
 	    #to disable old SysV init scripts.
-	    print "replacing ceph init script with own ceph.service\n";
+	    print "\nreplacing ceph init script with own ceph.service\n";
 	    eval {
 		PVE::Tools::run_command('cp -v /usr/share/doc/pve-manager/examples/ceph.service /etc/systemd/system/ceph.service');
 		PVE::Tools::run_command('systemctl daemon-reload');
 		PVE::Tools::run_command('systemctl enable ceph.service');
 	    };
-	    warn "could not install ceph.service\n" if $@;
+	    my $err = $@;
+	    if (!$err) {
+		print "\ninstalled ceph.service successfully\n";
+	    } else {
+		warn "could not install ceph.service\n";
+	    }
 	}
 
 	return undef;
