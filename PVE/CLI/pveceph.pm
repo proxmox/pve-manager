36d5ee12720839f10f2348f6e34bd0320faeea96
Thomas Lamprecht <t.lamprecht@proxmox.com>
2019-05-26 12:40:33 +0200

    api: install ceph: remove old cruft
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/CLI/pveceph.pm
+++ PVE/CLI/pveceph.pm
@@ -100,41 +100,7 @@ __PACKAGE__->register_method ({
 	    PVE::Tools::file_set_contents("/etc/apt/sources.list.d/ceph.list",
 		"deb http://download.proxmox.com/debian/ceph-luminous stretch main\n");
 	} else {
-	    # use fixed devel repo for now, because there is no officila repo for jessie
-	    my $devrepo = undef;
-
-	    my $keyurl = $devrepo ?
-		"https://git.ceph.com/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc" :
-		"https://git.ceph.com/?p=ceph.git;a=blob_plain;f=keys/release.asc";
-
-	    print "download and import ceph repository keys\n";
-
-	    # Note: wget on Debian wheezy cannot handle new ceph.com certificates, so
-	    # we use LWP::UserAgent
-	    #system("wget -q -O- '$keyurl'| apt-key add - 2>&1 >/dev/null") == 0 ||
-	    #die "unable to download ceph release key\n";
-
-	    my $tmp_key_file = "/tmp/ceph-release-keys.asc";
-	    my $ua = LWP::UserAgent->new(protocols_allowed => ['http', 'https'], timeout => 120);
-	    $ua->env_proxy;
-	    my $response = $ua->get($keyurl);
-	    if ($response->is_success) {
-		my $data = $response->decoded_content;
-		PVE::Tools::file_set_contents($tmp_key_file, $data);
-	    } else {
-		die "unable to download ceph release key: " . $response->status_line . "\n";
-	    }
-
-	    system("apt-key add $tmp_key_file 2>&1 >/dev/null") == 0 ||
-		die "unable to download ceph release key\n";
-
-	    unlink $tmp_key_file;
-
-	    my $source = $devrepo ?
-		"deb http://gitbuilder.ceph.com/ceph-deb-jessie-x86_64-basic/ref/$devrepo jessie main\n" :
-		"deb http://download.ceph.com/debian-$cephver jessie main\n";
-
-	    PVE::Tools::file_set_contents("/etc/apt/sources.list.d/ceph.list", $source);
+	    die "not implemented ceph version: $cephver";
 	}
 
 	print "update available package list\n";
