13f0b446e07e88baad568666a0d0b0757ee797a0
Thomas Lamprecht <t.lamprecht@proxmox.com>
2019-05-26 13:39:41 +0200

    ceph: install: run_command is imported, use directly
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/CLI/pveceph.pm
+++ PVE/CLI/pveceph.pm
@@ -118,9 +118,9 @@ __PACKAGE__->register_method ({
 	if (! -e '/etc/systemd/system/ceph.service') {
 	    print "\nreplacing ceph init script with own ceph.service\n";
 	    eval {
-		PVE::Tools::run_command('cp -v /usr/share/doc/pve-manager/examples/ceph.service /etc/systemd/system/ceph.service');
-		PVE::Tools::run_command('systemctl daemon-reload');
-		PVE::Tools::run_command('systemctl enable ceph.service');
+		run_command('cp -v /usr/share/doc/pve-manager/examples/ceph.service /etc/systemd/system/ceph.service');
+		run_command('systemctl daemon-reload');
+		run_command('systemctl enable ceph.service');
 	    };
 	    if (my $err = $@) {
 		warn "WARNING: could not install ceph.service: $@\n";
