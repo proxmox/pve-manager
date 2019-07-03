proxmox/pve-manager

commit d851d63ecc8cfb20880d611c904fdc2ff83ae3e2

ceph: init: only handle keyring if auth is cephx
if auth is 'none' there is no client keyring, so do not generate it and
do not write it into the config

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

PVE/API2/Ceph.pm
@@ -331,7 +331,9 @@ __PACKAGE__->register_method ({
 		#'osd pool default pgp num' => $pg_num,
 	    }
 
-	    $cfg->{client}->{keyring} = '/etc/pve/priv/$cluster.$name.keyring';
+	    if ($auth eq 'cephx') {
+		$cfg->{client}->{keyring} = '/etc/pve/priv/$cluster.$name.keyring';
+	    }
 
 	    if ($param->{pg_bits}) {
 		$cfg->{global}->{'osd pg bits'} = $param->{pg_bits};
@@ -349,7 +351,9 @@ __PACKAGE__->register_method ({
 
 	    cfs_write_file('ceph.conf', $cfg);
 
-	    PVE::Ceph::Tools::get_or_create_admin_keyring();
+	    if ($auth eq 'cephx') {
+		PVE::Ceph::Tools::get_or_create_admin_keyring();
+	    }
 	    PVE::Ceph::Tools::setup_pve_symlinks();
 	});
 

commit 50d5fd6ab0318bf4f2d061180b82dedf50311489

ceph: init: fix undef var access
Fixes: d851d63ecc8cfb20880d611c904fdc2ff83ae3e2
Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

PVE/API2/Ceph.pm
@@ -302,6 +302,8 @@ __PACKAGE__->register_method ({
 	    PVE::Ceph::Tools::check_ceph_installed('ceph_bin');
 	}
 
+	my $auth = $param->{disable_cephx} ? 'none' : 'cephx';
+
 	# simply load old config if it already exists
 	PVE::Cluster::cfs_lock_file('ceph.conf', undef, sub {
 	    my $cfg = cfs_read_file('ceph.conf');
@@ -313,8 +315,6 @@ __PACKAGE__->register_method ({
 
 		UUID::generate($uuid);
 		UUID::unparse($uuid, $fsid);
-
-		my $auth = $param->{disable_cephx} ? 'none' : 'cephx';
 
 		$cfg->{global} = {
 		    'fsid' => $fsid,