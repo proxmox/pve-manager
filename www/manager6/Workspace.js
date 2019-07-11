7a8653ceaa6a2ee4a7666326af95d02a809a45c4
Thomas Lamprecht <t.lamprecht@proxmox.com>
2019-07-11 21:59:05 +0200

    Revert "add beta text with link to bugtracker"
    This reverts commit 1e12b3179148b67ec6f94654c963f33a1a300bb0.

--- www/manager6/Workspace.js
+++ www/manager6/Workspace.js
@@ -299,10 +299,6 @@ Ext.define('PVE.StdWorkspace', {
 			    html: 'Virtual Environment'
 			},
 			{
-			    padding: 5,
-			    html: '<a href="https://bugzilla.proxmox.com" target="_blank">BETA</a>'
-			},
-			{
 			    xtype: 'pveGlobalSearchField',
 			    tree: rtree
 			},
