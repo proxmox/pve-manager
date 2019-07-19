da97bd3d8830bf77f74e53c1d377796bb0a85c8b
Aaron Lauterer <a.lauterer@proxmox.com>
2019-07-19 15:32:20 +0200

    Add edit dialog for audio device
    Signed-off-by: Aaron Lauterer <a.lauterer@proxmox.com>

---
+++ www/manager6/qemu/AudioEdit.js
@@ -0,0 +1,65 @@
+Ext.define('PVE.qemu.AudioInputPanel', {
+    extend: 'Proxmox.panel.InputPanel',
+    xtype: 'pveAudioInputPanel',
+
+    onlineHelp: 'qm_audio_device',
+
+    onGetValues: function(values) {
+	var ret = PVE.Parser.printPropertyString(values);
+	if (ret === '') {
+	    return {
+		'delete': 'audio0'
+	    };
+	}
+	return {
+	    audio0: ret
+	};
+    },
+
+    items: [{
+	name: 'device',
+	xtype: 'proxmoxKVComboBox',
+	value: 'ich9-intel-hda',
+	fieldLabel: gettext('Audio Device'),
+	comboItems: [
+	    ['ich9-intel-hda', 'ich9-intel-hda'],
+	    ['intel-hda', 'intel-hda'],
+	    ['AC97', 'AC97']
+	]
+    }, {
+	name: 'driver',
+	xtype: 'displayfield',
+	value: 'spice',
+	submitValue: true,
+	fieldLabel: gettext('Backend Driver'),
+    }]
+});
+
+Ext.define('PVE.qemu.AudioEdit', {
+    extend: 'Proxmox.window.Edit',
+
+    vmconfig: undefined,
+
+    subject: gettext('Audio Device'),
+
+    items: [{
+	xtype: 'pveAudioInputPanel'
+    }],
+
+    initComponent : function() {
+	var me = this;
+
+	me.callParent();
+
+	me.load({
+	    success: function(response) {
+		me.vmconfig = response.result.data;
+
+		var audio0 = me.vmconfig.audio0;
+		if (audio0) {
+		    me.setValues(PVE.Parser.parsePropertyString(audio0));
+		}
+	    }
+	});
+    }
+});
