f1e925727420a9970083f4ebe24aaaa9edbc1c18
Thomas Lamprecht <t.lamprecht@proxmox.com>
2019-12-03 14:29:50 +0100

    ui: ha: drop unused pve-ha-resources model
    we only have the unified pve-ha-status model now
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/ha/Resources.js
+++ www/manager6/ha/Resources.js
@@ -189,16 +189,4 @@ Ext.define('PVE.ha.ResourcesView', {
 
 	me.callParent();
     }
-}, function() {
-
-    Ext.define('pve-ha-resources', {
-	extend: 'Ext.data.Model',
-	fields: [ 
-	  'sid', 'state', 'digest', 'errors', 'group', 'comment',
-	  'max_restart', 'max_relocate', 'type', 'status', 'node',
-	  'crm_state', 'request_state'
-	],
-	idProperty: 'sid'
-    });
-
 });
