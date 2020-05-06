c00c4207ea046a0e489f9ec571173394d61a4ee9
Dominik Csapak <d.csapak@proxmox.com>
2020-05-06 16:31:08 +0200

    ui: add ACME selector formfields for account and plugins
    filter the plugins by type === 'dns'
    and add a convenience method for ACMEAccountSelector to check if there
    are any accounts
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ www/manager6/form/ACMEAccountSelector.js
@@ -0,0 +1,21 @@
+Ext.define('PVE.form.ACMEAccountSelector', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.pveACMEAccountSelector',
+
+    displayField: 'name',
+    valueField: 'name',
+
+    store: {
+	model: 'pve-acme-accounts',
+	autoLoad: true,
+    },
+
+    triggerAction: 'all',
+    queryMode: 'local',
+    allowBlank: false,
+    editable: false,
+
+    isEmpty: function() {
+	return this.getStore().getData().length === 0;
+    }
+});
---
+++ www/manager6/form/ACMEPluginSelector.js
@@ -0,0 +1,19 @@
+Ext.define('PVE.form.ACMEPluginSelector', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.pveACMEPluginSelector',
+
+    fieldLabel: gettext('Plugin'),
+    displayField: 'plugin',
+    valueField: 'plugin',
+
+    store: {
+	model: 'pve-acme-plugins',
+	autoLoad: true,
+	filters: item => item.data.type === 'dns',
+    },
+
+    triggerAction: 'all',
+    queryMode: 'local',
+    allowBlank: false,
+    editable: false,
+});
