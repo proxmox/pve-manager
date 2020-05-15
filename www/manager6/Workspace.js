d1efcadf38af5474a06b9471c6efa8c9767595da
Dominik Csapak <d.csapak@proxmox.com>
2020-05-15 10:19:27 +0200

    ui: use PMX.image.Logo from widget-toolkit
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/manager6/Workspace.js
+++ www/manager6/Workspace.js
@@ -307,8 +307,7 @@ Ext.define('PVE.StdWorkspace', {
 		    margin: '2 0 2 5',
 		    items: [
 			{
-			    html: '<a class="x-unselectable" target=_blank href="https://www.proxmox.com">' +
-				'<img style="padding-top:4px;padding-right:5px" src="/pve2/images/proxmox_logo.png"/></a>'
+			    xtype: 'proxmoxlogo',
 			},
 			{
 			    minWidth: 150,
