5d0d14f179a740a13359d002f7e030665ee0f21e
Stoiko Ivanov <s.ivanov@proxmox.com>
2020-07-14 10:09:19 +0200

    VZDump: add TARFILE to environment for hookscripts
    The renaming of tarfile to target in 6cba1855d801545df3f3f81619dfa11cf2501858
    can break existing vzdump hook scripts of users.
    by setting the TARFILE variable in addition to TARGET the scripts will continue
    to work.
    
    Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

--- PVE/VZDump.pm
+++ PVE/VZDump.pm
@@ -590,6 +590,8 @@ sub run_hook_script {
     foreach my $ek (qw(vmtype hostname target logfile)) {
 	$ENV{uc($ek)} = $task->{$ek} if $task->{$ek};
     }
+    # FIXME: for backwards compatibility - drop with PVE 7.0
+    $ENV{TARFILE} = $task->{target} if $task->{target};
 
     run_command ($logfd, $cmd);
 }
