a88c03c8208f8141291ac6809908b4e842ddff0e
Thomas Lamprecht <t.lamprecht@proxmox.com>
2020-10-28 09:52:30 +0100

    ui: window notes: refactor and code cleanup
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/window/NotesEdit.js
+++ www/manager6/window/NotesEdit.js
@@ -1,27 +1,22 @@
 Ext.define('PVE.window.NotesEdit', {
     extend: 'Proxmox.window.Edit',
 
-    initComponent : function() {
-	var me = this;
+    title: gettext('Notes'),
 
-	Ext.apply(me, {
-	    title: gettext('Notes'),
-	    width: 600,
-	    height: '400px',
-	    resizable: true,
-	    layout: 'fit',
-	    defaultButton: undefined,
-	    items: {
-		xtype: 'textarea',
-		name: 'description',
-		height: '100%',
-		value: '',
-		hideLabel: true
-	    }
-	});
+    width: 600,
+    height: '400px',
+    resizable: true,
+    layout: 'fit',
 
-	me.callParent();
+    autoLoad: true,
 
-	me.load();
-    }
+    defaultButton: undefined,
+
+    items: {
+	xtype: 'textarea',
+	name: 'description',
+	height: '100%',
+	value: '',
+	hideLabel: true,
+    },
 });
