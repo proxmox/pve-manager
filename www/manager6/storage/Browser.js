d2ac54517c80ab0214e6ab24dc7d5bc4881747a3
Fabian Ebner <f.ebner@proxmox.com>
2020-11-18 11:04:18 +0100

    ui: storage: get content types from resources
    to avoid waiting for a status API call.
    
    Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

--- www/manager6/storage/Browser.js
+++ www/manager6/storage/Browser.js
@@ -38,89 +38,81 @@ Ext.define('PVE.storage.Browser', {
 	    hstateid: 'storagetab'
 	});
 
-	// call here, so there is a root for insertNodes()
-	me.callParent();
-
 	if (caps.storage['Datastore.Allocate'] ||
 	    caps.storage['Datastore.AllocateSpace'] ||
 	    caps.storage['Datastore.Audit']) {
+	    let storageInfo = PVE.data.ResourceStore.findRecord(
+		'id',
+		`storage/${nodename}/${storeid}`,
+	    );
+	    let contents = storageInfo.data.content.split(',');
 
-	    Proxmox.Utils.API2Request({
-		url: `/nodes/${nodename}/storage/${storeid}/status`,
-		method: 'GET',
-		success: function(response, opts) {
-		    var contents = response.result.data.content.split(',');
-		    var items = [];
-
-		    if (contents.includes('backup')) {
-			items.push({
-			    xtype: 'pveStorageBackupView',
-			    title: gettext('Backups'),
-			    iconCls: 'fa fa-floppy-o',
-			    itemId: 'contentBackup',
-			    hasCommentColumn: true,
-			});
-		    }
-		    if (contents.includes('images')) {
-			items.push({
-			    xtype: 'pveStorageImageView',
-			    title: gettext('VM Disks'),
-			    iconCls: 'fa fa-hdd-o',
-			    itemId: 'contentImages',
-			    content: 'images',
-			});
-		    }
-		    if (contents.includes('rootdir')) {
-			items.push({
-			    xtype: 'pveStorageImageView',
-			    title: gettext('CT Volumes'),
-			    iconCls: 'fa fa-hdd-o lxc',
-			    itemId: 'contentRootdir',
-			    content: 'rootdir',
-			});
-		    }
-		    if (contents.includes('iso')) {
-			items.push({
-			    xtype: 'pveStorageContentView',
-			    title: gettext('ISO Images'),
-			    iconCls: 'pve-itype-treelist-item-icon-cdrom',
-			    itemId: 'contentIso',
-			    content: 'iso',
-			    useUploadButton: true,
-			});
-		    }
-		    if (contents.includes('vztmpl')) {
-			items.push({
-			    xtype: 'pveStorageTemplateView',
-			    title: gettext('CT Templates'),
-			    iconCls: 'fa fa-file-o lxc',
-			    itemId: 'contentVztmpl',
-			});
-		    }
-		    if (contents.includes('snippets')) {
-			items.push({
-			    xtype: 'pveStorageContentView',
-			    title: gettext('Snippets'),
-			    iconCls: 'fa fa-file-code-o',
-			    itemId: 'contentSnippets',
-			    content: 'snippets',
-			});
-		    }
-		    me.insertNodes(items);
-		},
-	    });
+	    if (contents.includes('backup')) {
+		me.items.push({
+		    xtype: 'pveStorageBackupView',
+		    title: gettext('Backups'),
+		    iconCls: 'fa fa-floppy-o',
+		    itemId: 'contentBackup',
+		    hasCommentColumn: true,
+		});
+	    }
+	    if (contents.includes('images')) {
+		me.items.push({
+		    xtype: 'pveStorageImageView',
+		    title: gettext('VM Disks'),
+		    iconCls: 'fa fa-hdd-o',
+		    itemId: 'contentImages',
+		    content: 'images',
+		});
+	    }
+	    if (contents.includes('rootdir')) {
+		me.items.push({
+		    xtype: 'pveStorageImageView',
+		    title: gettext('CT Volumes'),
+		    iconCls: 'fa fa-hdd-o lxc',
+		    itemId: 'contentRootdir',
+		    content: 'rootdir',
+		});
+	    }
+	    if (contents.includes('iso')) {
+		me.items.push({
+		    xtype: 'pveStorageContentView',
+		    title: gettext('ISO Images'),
+		    iconCls: 'pve-itype-treelist-item-icon-cdrom',
+		    itemId: 'contentIso',
+		    content: 'iso',
+		    useUploadButton: true,
+		});
+	    }
+	    if (contents.includes('vztmpl')) {
+		me.items.push({
+		    xtype: 'pveStorageTemplateView',
+		    title: gettext('CT Templates'),
+		    iconCls: 'fa fa-file-o lxc',
+		    itemId: 'contentVztmpl',
+		});
+	    }
+	    if (contents.includes('snippets')) {
+		me.items.push({
+		    xtype: 'pveStorageContentView',
+		    title: gettext('Snippets'),
+		    iconCls: 'fa fa-file-code-o',
+		    itemId: 'contentSnippets',
+		    content: 'snippets',
+		});
+	    }
 	}
 
 	if (caps.storage['Permissions.Modify']) {
-	    me.insertNodes([
-		{
-		    xtype: 'pveACLView',
-		    title: gettext('Permissions'),
-		    iconCls: 'fa fa-unlock',
-		    itemId: 'permissions',
-		    path: '/storage/' + storeid
-		},
-	    ]);
+	    me.items.push({
+		xtype: 'pveACLView',
+		title: gettext('Permissions'),
+		iconCls: 'fa fa-unlock',
+		itemId: 'permissions',
+		path: '/storage/' + storeid
+	    });
 	}
+
+	me.callParent();
    }
 });
