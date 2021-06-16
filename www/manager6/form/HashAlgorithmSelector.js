6b3e76058124f663beb8aaf5969dcd0a8348cda3
Lorenz Stechauner <l.stechauner@proxmox.com>
2021-06-16 11:36:02 +0200

    ui: add HashAlgorithmSelector
    Signed-off-by: Lorenz Stechauner <l.stechauner@proxmox.com>

---
+++ www/manager6/form/HashAlgorithmSelector.js
@@ -0,0 +1,16 @@
+Ext.define('PVE.form.hashAlgorithmSelector', {
+    extend: 'Proxmox.form.KVComboBox',
+    alias: ['widget.pveHashAlgorithmSelector'],
+    config: {
+	deleteEmpty: false,
+    },
+    comboItems: [
+	['__default__', 'None'],
+	['md5', 'MD5'],
+	['sha1', 'SHA-1'],
+	['sha224', 'SHA-224'],
+	['sha256', 'SHA-256'],
+	['sha384', 'SHA-384'],
+	['sha512', 'SHA-512'],
+    ],
+});
