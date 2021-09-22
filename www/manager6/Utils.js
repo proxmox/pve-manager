3b5617cc2fbc48e68c8e4d62137743e2e100c8ab
Dominik Csapak <d.csapak@proxmox.com>
2021-09-22 11:27:45 +0200

    ui: Utils: add capture group for the id in bus_match
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

--- www/manager6/Utils.js
+++ www/manager6/Utils.js
@@ -13,7 +13,7 @@ Ext.define('PVE.Utils', {
 
     toolkit: undefined, // (extjs|touch), set inside Toolkit.js
 
-    bus_match: /^(ide|sata|virtio|scsi)\d+$/,
+    bus_match: /^(ide|sata|virtio|scsi)(\d+)$/,
 
     log_severity_hash: {
 	0: "panic",
