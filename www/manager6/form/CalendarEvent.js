ec23a5cb32f85d77045d45950a92f19b87b1a581
Thomas Lamprecht <t.lamprecht@proxmox.com>
2021-12-07 18:47:44 +0100

    ui: calendar event: add once daily example and clarify workday one
    Using 00:00 with relying on the implied default is sub optimal as its
    a bit of a magic example that new users may not understand as easily.
    So spell it out explicitly, even if there'd be a shorter version
    possible.
    
    We also had some request for the once-daily every day, and its a
    sensible example to have in general, could help getting the
    difference between an hour list and a single one.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/form/CalendarEvent.js
+++ www/manager6/form/CalendarEvent.js
@@ -18,8 +18,9 @@ Ext.define('PVE.form.CalendarEvent', {
 	data: [
 	    { value: '*/30', text: Ext.String.format(gettext("Every {0} minutes"), 30) },
 	    { value: '*/2:00', text: gettext("Every two hours") },
+	    { value: '21:00', text: gettext("Every day") + " 21:00" },
 	    { value: '2,22:30', text: gettext("Every day") + " 02:30, 22:30" },
-	    { value: 'mon..fri', text: gettext("Monday to Friday") + " 00:00" },
+	    { value: 'mon..fri 00:00', text: gettext("Monday to Friday") + " 00:00" },
 	    { value: 'mon..fri */1:00', text: gettext("Monday to Friday") + ': ' + gettext("hourly") },
 	    {
 		value: 'mon..fri 7..18:00/15',
