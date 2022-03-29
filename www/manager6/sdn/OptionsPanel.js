c35bbd6ef7260b58aad6b8ca176aa02a43e6ba03
Thomas Lamprecht <t.lamprecht@proxmox.com>
2022-03-29 17:37:27 +0200

    ui: sdn: options: flex all and drop collapsible
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/sdn/OptionsPanel.js
+++ www/manager6/sdn/OptionsPanel.js
@@ -11,31 +11,25 @@ Ext.define('PVE.sdn.Options', {
 
     onlineHelp: 'pvesdn_config_controllers',
 
-    initComponent: function() {
-	var me = this;
-
-	me.items = [
+    items: [
 	{
 	    xtype: 'pveSDNControllerView',
 	    title: gettext('Controllers'),
-	    border: 0,
-	    collapsible: true,
+	    flex: 1,
 	    padding: '0 0 20 0',
+	    border: 0,
 	},
 	{
 	    xtype: 'pveSDNIpamView',
 	    title: 'IPAMs',
-	    border: 0,
-	    collapsible: true,
+	    flex: 1,
 	    padding: '0 0 20 0',
+	    border: 0,
 	}, {
 	    xtype: 'pveSDNDnsView',
-	    flex: 1,
-	    collapsible: true,
 	    title: 'DNS',
+	    flex: 1,
 	    border: 0,
-	}];
-
-	me.callParent();
-    },
+	},
+    ],
 });
