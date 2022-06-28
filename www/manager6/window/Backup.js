proxmox/pve-manager

commit c4dca88b5e6e3fa054bd1aea295183a6fcb3afc1

ui: manual backup: also set notes-template default
like is done for other vzdump options already.

Requested in the community forum:
https://forum.proxmox.com/threads/108970/#post-468655

Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

www/manager6/window/Backup.js
@@ -113,6 +113,10 @@ Ext.define('PVE.window.Backup', {
 			    if (!initialDefaults && data.mode !== undefined) {
 				modeSelector.setValue(data.mode);
 			    }
+			    if (!initialDefaults && (data['notes-template'] ?? false)) {
+				me.down('field[name=notes-template]')
+				    .setValue(data['notes-template']);
+			    }
 
 			    initialDefaults = true;
 

commit 6a3052b2994c94baf0a61b3d177e200c421c8fd3

ui: manual backup: unescape notes template on load
It's escaped in the API result and will be re-escaped upon submit,
leading to confusion as reported in the community forum:
https://forum.proxmox.com/threads/110747/post-480507

Fixes: c4dca88b ("ui: manual backup: also set notes-template default")
Signed-off-by: Fabian Ebner <f.ebner@proxmox.com>

www/manager6/window/Backup.js
@@ -114,8 +114,9 @@ Ext.define('PVE.window.Backup', {
 				modeSelector.setValue(data.mode);
 			    }
 			    if (!initialDefaults && (data['notes-template'] ?? false)) {
-				me.down('field[name=notes-template]')
-				    .setValue(data['notes-template']);
+				me.down('field[name=notes-template]').setValue(
+				    PVE.Utils.unEscapeNotesTemplate(data['notes-template']),
+				);
 			    }
 
 			    initialDefaults = true;