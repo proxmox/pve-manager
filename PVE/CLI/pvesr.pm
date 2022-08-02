proxmox/pve-manager

commit d8cd8e8cf9795dc9c2462a67e9ef89ad31759796

pvesr: prepare_local_job: use PVE::replication::prepare
to reduce code duplication. this slightly changes behaviour
compared to the previous version:

only disks with the correct prefix are cleaned up, not all
disks with __replication* snapshots.

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

PVE/CLI/pvesr.pm
@@ -129,31 +129,17 @@ __PACKAGE__->register_method ({
 	my $snapname = PVE::ReplicationState::replication_snapshot_name($jobid, $last_sync);
 
 	# find replication snapshots
-	my $last_snapshots = {};
 	foreach my $storeid (@$storage_list) {
 	    my $scfg = PVE::Storage::storage_config($storecfg, $storeid);
 	    my $plugin = PVE::Storage::Plugin->lookup($scfg->{type});
 	    my $images = $plugin->list_images($storeid, $scfg, $vmid, undef, $cache);
-	    foreach my $disk (@$images) {
-		my $volid = $disk->{volid};
-		my ($storeid, $volname) = PVE::Storage::parse_volume_id($volid);
-		my $list = $plugin->volume_snapshot_list($scfg, $storeid, $volname); # fixme: pass $cache
-		my $found_replication_snapshots = 0;
-		foreach my $snap (@$list) {
-		    if ($snap eq $snapname || (defined($parent_snapname) && ($snap eq $parent_snapname))) {
-			$last_snapshots->{$volid}->{$snap} = 1 if $wanted_volids->{$volid};
-		    } elsif ($snap =~ m/^__replication_/) {
-			$found_replication_snapshots = 1;
-			if ($wanted_volids->{$volid}) {
-			    $logfunc->("$jobid: delete stale replication snapshot '$snap' on $volid");
-			    PVE::Storage::volume_snapshot_delete($storecfg, $volid, $snap);
-			}
-		    }
-		}
-		# remove stale volumes
-		if ($found_replication_snapshots && !$wanted_volids->{$volid}) {
+	    my $volids = [ map { $_->{volid} } @$images ];
+	    my ($last_snapshots, $cleaned_replicated_volumes) = PVE::Replication::prepare($storecfg, $volids, $jobid, $last_sync, $parent_snapname, $logfunc);
+	    foreach my $volid (keys %$cleaned_replicated_volumes) {
+		if (!$wanted_volids->{$volid}) {
 		    $logfunc->("$jobid: delete stale volume '$volid'");
 		    PVE::Storage::vdisk_free($storecfg, $volid);
+		    delete $last_snapshots->{$volid};
 		}
 	    }
 	}

commit 55b1af44fd8fec28c2415a3c30a72dd8c29a5388

pvesr: prepare local job: remove stale replicated volumes immediately
Commit d8cd8e8cf9795dc9c2462a67e9ef89ad31759796 introduced a
regression where only stale replicated volumes with an older
timestamp would be cleaned up. This meant that after removing a volume
from the guest config, it would only be cleaned up the second time the
replication ran afterwards. And the volume could become completely
orphaned in case the relevant storage wasn't used by the job anymore.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/CLI/pvesr.pm
@@ -137,8 +137,18 @@ __PACKAGE__->register_method ({
 	    push @$volids, map { $_->{volid} } @$images;
 	}
 	my ($local_snapshots, $cleaned_replicated_volumes) = PVE::Replication::prepare($storecfg, $volids, $jobid, $last_sync, $parent_snapname, $logfunc);
-	foreach my $volid (keys %$cleaned_replicated_volumes) {
-	    if (!$wanted_volids->{$volid}) {
+	for my $volid ($volids->@*) {
+	    next if $wanted_volids->{$volid};
+
+	    my $stale = $cleaned_replicated_volumes->{$volid};
+	    # prepare() will not remove the last_sync snapshot, but if the volume was used by the
+	    # job and is not wanted anymore, it is stale too. And not removing it now might cause
+	    # it to be missed later, because the relevant storage might not get scanned anymore.
+	    $stale ||= grep {
+		PVE::Replication::is_replication_snapshot($_, $jobid)
+	    } keys %{$local_snapshots->{$volid} // {}};
+
+	    if ($stale) {
 		$logfunc->("$jobid: delete stale volume '$volid'");
 		PVE::Storage::vdisk_free($storecfg, $volid);
 		delete $local_snapshots->{$volid};