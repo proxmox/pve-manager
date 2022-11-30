40946b6c605fb5ff09321df574f51081fcc52f74
Thomas Lamprecht <t.lamprecht@proxmox.com>
2022-11-30 16:20:58 +0100

    appliance info: update apltest for zstd based archives
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- aplinfo/apltest.pl
+++ aplinfo/apltest.pl
@@ -17,7 +17,7 @@ foreach my $k (keys %{$pkglist->{'all'}}) {
     # heuristic only..
     my $template = "$res->{package}_$res->{version}_$res->{architecture}.tar";
 
-    if ($k !~ m/^($res->{os}-)?\Q$template\E\.(gz|xz)$/) {
+    if ($k !~ m/^($res->{os}-)?\Q$template\E\.(gz|xz|zst)$/) {
 	print "ERROR: $k != $template\n";
 	#print Dumper($res) . "\n";
 	$err = 1;
