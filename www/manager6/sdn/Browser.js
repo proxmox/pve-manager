21820ede018d0126973df7983107c940bf34cbac
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-01-10 13:51:38 +0100

    ui: sdn browser: code cleanup/modernization
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/sdn/Browser.js
+++ www/manager6/sdn/Browser.js
@@ -5,27 +5,25 @@ Ext.define('PVE.sdn.Browser', {
     onlineHelp: 'chapter_pvesdn',
 
     initComponent: function() {
-	var me = this;
+	let me = this;
 
-	var nodename = me.pveSelNode.data.node;
+	let nodename = me.pveSelNode.data.node;
 	if (!nodename) {
 	    throw "no node name specified";
 	}
-
-	var sdnid = me.pveSelNode.data.sdn;
-	if (!sdnid) {
+	let sdnId = me.pveSelNode.data.sdn;
+	if (!sdnId) {
 	    throw "no sdn ID specified";
 	}
 
 	me.items = [];
 
-	var caps = Ext.state.Manager.get('GuiCap');
-
 	Ext.apply(me, {
-	    title: Ext.String.format(gettext("Zone {0} on node {1}"),
-				     "'" + sdnid + "'", "'" + nodename + "'"),
+	    title: Ext.String.format(gettext("Zone {0} on node {1}"), `'${sdnId}'`, `'${nodename}'`),
 	    hstateid: 'sdntab',
 	});
+
+	const caps = Ext.state.Manager.get('GuiCap');
 
 	if (caps.sdn['SDN.Audit']) {
 	    me.items.push({
@@ -35,14 +33,13 @@ Ext.define('PVE.sdn.Browser', {
 		itemId: 'content',
 	    });
 	}
-
 	if (caps.sdn['Permissions.Modify']) {
 	    me.items.push({
 		xtype: 'pveACLView',
 		title: gettext('Permissions'),
 		iconCls: 'fa fa-unlock',
 		itemId: 'permissions',
-		path: '/sdn/zones/' + sdnid,
+		path: `/sdn/zones/${sdnId}`,
 	    });
 	}
 
