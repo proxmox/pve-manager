a4240b80e8288b92074164750d132e2ee72a907c
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-01-19 16:41:21 +0100

    ui: backup info: make initial height dependent of body view-height
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/dc/Backup.js
+++ www/manager6/dc/Backup.js
@@ -548,7 +548,7 @@ Ext.define('PVE.dc.BackupView', {
 	    Ext.create('Ext.window.Window', {
 		modal: true,
 		width: 800,
-		height: 600,
+		height: Ext.getBody().getViewSize().height > 1000 ? 800 : 600, // factor out as common infra?
 		resizable: true,
 		layout: 'fit',
 		title: gettext('Backup Details'),
