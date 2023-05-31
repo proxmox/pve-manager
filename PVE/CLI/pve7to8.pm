5af851c5afe6fe25b253a19543e62b4cdfda508f
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-05-31 10:19:27 +0200

    pve7to8: update check for free space to >= 5 GB
    As for persistent storage we normally use GB not GiB nowadays (e.g.,
    in the Web UI), as 2^30 is bigger than 10^9, rounding up to 5 GB
    should be fine; it's really safer to have a bit more free space as
    minimally required in some minimal setups.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/CLI/pve7to8.pm
+++ PVE/CLI/pve7to8.pm
@@ -1076,8 +1076,8 @@ sub check_misc {
     $log_systemd_unit_state->('pvestatd.service');
 
     my $root_free = PVE::Tools::df('/', 10);
-    log_warn("Less than 4 GiB free space on root file system.")
-	if defined($root_free) && $root_free->{avail} < 4*1024*1024*1024;
+    log_warn("Less than 5 GB free space on root file system.")
+	if defined($root_free) && $root_free->{avail} < 5 * 1000*1000*1000;
 
     log_info("Checking for running guests..");
     my $running_guests = 0;
