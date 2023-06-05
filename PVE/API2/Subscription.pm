31e5ac131ca7b06ec837dad4321dba346b67c431
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-06-05 19:13:17 +0200

    api subscription: ensure access to ceph enterprise repo for upgrade
    For upgrade to Proxmox VE 8, which added this repo. This shouldn't
    change anything for current release.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/API2/Subscription.pm
+++ PVE/API2/Subscription.pm
@@ -83,6 +83,14 @@ sub write_etc_subscription {
     my $server_id = PVE::API2Tools::get_hwaddress();
     mkdir "/etc/apt/auth.conf.d";
     Proxmox::RS::Subscription::write_subscription($filename, "/etc/apt/auth.conf.d/pve.conf", "enterprise.proxmox.com/debian/pve", $info);
+
+    # NOTE: preparation for easier upgrade to Proxmox VE 8, which introduced the ceph enterprise repo
+    my $ceph_auth = '';
+    for my $ceph_release ('quincy', 'reef') {
+	$ceph_auth .= "machine enterprise.proxmox.com/debian/ceph-${ceph_release}"
+	    ." login $info->{key} password $info->{serverid}\n"
+    }
+    PVE::Tools::file_set_contents("/etc/apt/auth.conf.d/ceph.conf", $ceph_auth);
 }
 
 __PACKAGE__->register_method ({
