63c8b37122a7d9b83c26fc3c4c31371e7a9c0bcf
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-06-07 10:59:12 +0200

    appliances: switch over to Proxmox VE 8 index
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/APLInfo.pm
+++ PVE/APLInfo.pm
@@ -198,7 +198,7 @@ sub get_apl_sources {
 	{
 	    host => "download.proxmox.com",
 	    url => "http://download.proxmox.com/images",
-	    file => 'aplinfo-pve-7.dat',
+	    file => 'aplinfo-pve-8.dat',
 	},
 	{
 	    host => "releases.turnkeylinux.org",
