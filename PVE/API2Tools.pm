proxmox/pve-manager

commit f6395eb69cc19bdb0ea6000f653609cf0688cc87

hw-address: check if source file changed so cache needs update
We cache the hash of this file, it makes sense to first check if the
file changed via `stat` and recompute the hash if needed.

This mirrors similar changes done for PMG [0].

[0]: https://git.proxmox.com/?p=pmg-api.git;a=commit;h=16d2ff9f8e90db64114a66d78672f5a03f5ee990.

Signed-off-by: Maximiliano Sandoval <m.sandoval@proxmox.com>

PVE/API2Tools.pm
@@ -17,14 +17,22 @@ use PVE::SafeSyslog;
 use PVE::Storage::Plugin;
 
 my $hwaddress;
+my $hwaddress_st = {};
 
 sub get_hwaddress {
-
-    return $hwaddress if defined ($hwaddress);
-
     my $fn = '/etc/ssh/ssh_host_rsa_key.pub';
+    my $st = stat($fn);
+
+    if (defined($hwaddress)
+	&& $hwaddress_st->{mtime} == $st->mtime
+	&& $hwaddress_st->{ino} == $st->ino
+	&& $hwaddress_st->{dev} == $st->dev) {
+	return $hwaddress;
+    }
+
     my $sshkey = PVE::Tools::file_get_contents($fn);
     $hwaddress = uc(md5_hex($sshkey));
+    $hwaddress_st->@{'mtime', 'ino', 'dev'} = ($st->mtime, $st->ino, $st->dev);
 
     return $hwaddress;
 }

commit fddf562bf9403c2b2c4f8cb7fd4ed502149637a7

api tools: fix usage of stat by-name interface
Fixes: f6395eb6 ("hw-address: check if source file changed so cache needs update")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2Tools.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Digest::MD5 qw(md5_hex);
+use File::stat;
 use Net::IP;
 use URI::Escape;
 use URI;