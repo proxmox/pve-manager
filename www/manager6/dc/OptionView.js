fea56c69dde04fce4b5605f07cc76b131004adad
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-10-29 18:02:20 +0100

    ui: datacenter config: adapt to new default MAC prefix
    The official Proxmox OUI, assigned by the IEEE, is now used as
    default.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/dc/OptionView.js
+++ www/manager6/dc/OptionView.js
@@ -94,7 +94,7 @@ Ext.define('PVE.dc.OptionView', {
 	me.add_text_row('mac_prefix', gettext('MAC address prefix'), {
 	    deleteEmpty: true,
 	    vtype: 'MacPrefix',
-	    defaultValue: Proxmox.Utils.noneText,
+	    defaultValue: 'BC:24:11',
 	});
 	me.add_inputpanel_row('migration', gettext('Migration Settings'), {
 	    renderer: PVE.Utils.render_as_property_string,
