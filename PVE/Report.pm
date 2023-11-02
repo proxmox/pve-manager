6e167f9a9a46e52dfe83aa009729d59bbc7a65ea
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-11-02 20:59:49 +0100

    report: add hint for dir2text
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- PVE/Report.pm
+++ PVE/Report.pm
@@ -10,7 +10,7 @@ my sub dir2text {
     my ($target_dir, $regexp) = @_;
 
     print STDERR "dir2text '${target_dir}${regexp}'...";
-    my $text = '';
+    my $text = "# output '${target_dir}${regexp}' file(s)\n";
     PVE::Tools::dir_glob_foreach($target_dir, $regexp, sub {
 	my ($file) = @_;
 	return if $file eq '.' || $file eq '..';
