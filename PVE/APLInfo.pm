proxmox/pve-manager

commit 805cae93fdee05a5a9ca95583b860762573ef977

appliance index: rotate update log if bigger than 256 KiB
50 KiB is really tiny and while this log file is not _that_ important,
spending a few KiB more to keep much older logs around is almost
always a win-win situation, now that root storage size is rather some
hundred GB in most setups.

Drop the unused size variable while touching this, which was the
actual thing that made me look closer here.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/APLInfo.pm
@@ -213,8 +213,7 @@ sub get_apl_sources {
 sub update {
     my ($proxy) = @_;
 
-    my $size;
-    if (($size = (-s $logfile) || 0) > (1024*50)) {
+    if (-s $logfile || 0 > 1024 * 256) {
 	rename($logfile, "$logfile.0");
     }
     my $logfd = IO::File->new (">>$logfile");

commit 11064d3263b9a3620c4ca43cded829e62b235de9

appliance index: fix precedence in size check for log rotation
In Perl, > takes precedence over ||, see perldoc perlop, so currently
the check will trigger with any size.

Fixes: 805cae93 ("appliance index: rotate update log if bigger than 256 KiB")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

PVE/APLInfo.pm
@@ -213,7 +213,7 @@ sub get_apl_sources {
 sub update {
     my ($proxy) = @_;
 
-    if (-s $logfile || 0 > 1024 * 256) {
+    if ((-s $logfile || 0) > 1024 * 256) {
 	rename($logfile, "$logfile.0");
     }
     my $logfd = IO::File->new (">>$logfile");