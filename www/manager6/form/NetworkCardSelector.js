74a241953061a2c10117ec2268cba3891b64006a
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-03-27 11:38:39 +0100

    ui: network card selector: add Intel E1000E as choosable model
    it's popular in VMware ESXi and so should be available to choose
    directly, avoiding a invalid model field in the import wizard.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/form/NetworkCardSelector.js
+++ www/manager6/form/NetworkCardSelector.js
@@ -3,6 +3,7 @@ Ext.define('PVE.form.NetworkCardSelector', {
     alias: 'widget.pveNetworkCardSelector',
     comboItems: [
 	['e1000', 'Intel E1000'],
+	['e1000e', 'Intel E1000E'],
 	['virtio', 'VirtIO (' + gettext('paravirtualized') + ')'],
 	['rtl8139', 'Realtek RTL8139'],
 	['vmxnet3', 'VMware vmxnet3'],
