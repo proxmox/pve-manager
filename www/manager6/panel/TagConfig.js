22fa3fc3837656128b58e0427b59908f33306a43
Thomas Lamprecht <t.lamprecht@proxmox.com>
2024-11-10 12:12:55 +0100

    ui: tag navigation config: link to generic UI chapter for now
    To avoid the need to bump pve-docs for now.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/panel/TagConfig.js
+++ www/manager6/panel/TagConfig.js
@@ -2,5 +2,6 @@ Ext.define('PVE.panel.TagConfig', {
     extend: 'PVE.panel.Config',
     alias: 'widget.pveTagConfig',
 
-    onlineHelp: 'gui_tags',
+    //onlineHelp: 'gui_tags', // TODO: use this one once available
+    onlineHelp: 'chapter_gui',
 });
