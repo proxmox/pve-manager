proxmox/pve-manager

commit a3fe9c54101e49f5ccdf8d91f1ba5402e70a8d28

api: notifications: require powerful privileges for target management
These allow one to indirectly access resources from the POV of the
Proxmox VE cluster nodes. While gotify is relatively harmless, smtp
could already cause more problems to admins that are not aware of the
implications of allowing users to add targets while having some open
smtp relay that is only accessible from networks the PVE nodes can
access but not the user that can talk with PVE's API. The webhook one
is then pretty much free-form and might cause some adverse effects in
environments that are only loosely guarded, and while that might point
at general security problems, it's likely that admins will still place
the blame at our projects.

So while the former should not be problematic, the new not yet fully
released webhooks could have some impact. That said, it currently
requires Mapping.Modify, which is a intermediate powerful level priv,
so it's not like any user could use this. Still, hedging for the
safer side here seems the better choice for now, we still can open
this up if there's user feedback and we deem it safe enough doing so.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2/Cluster/Notifications.pm
@@ -500,7 +500,15 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new sendmail endpoint',
     permissions => {
-	check => ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
+	],
     },
     parameters => {
 	additionalProperties => 0,
@@ -548,7 +556,15 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing sendmail endpoint',
     permissions => {
-	check => ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
+	],
     },
     parameters => {
 	additionalProperties => 0,
@@ -764,7 +780,15 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new gotify endpoint',
     permissions => {
-	check => ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
+	],
     },
     parameters => {
 	additionalProperties => 0,
@@ -808,7 +832,15 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing gotify endpoint',
     permissions => {
-	check => ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
+	],
     },
     parameters => {
 	additionalProperties => 0,
@@ -1067,8 +1099,14 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new smtp endpoint',
     permissions => {
-	check => ['or',
-	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
 	],
     },
     parameters => {
@@ -1127,8 +1165,14 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing smtp endpoint',
     permissions => {
-	check => ['or',
-	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
 	],
     },
     parameters => {
@@ -1379,7 +1423,15 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new webhook endpoint',
     permissions => {
-	check => ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
+	],
     },
     parameters => {
 	additionalProperties => 0,
@@ -1412,7 +1464,15 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing webhook endpoint',
     permissions => {
-	check => ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	check => [
+	    ['and',
+		['perm', '/mapping/notifications', ['Mapping.Modify']],
+		['or',
+		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
+		],
+	    ],
+	],
     },
     parameters => {
 	additionalProperties => 0,

commit 369a24303c063e89803c248b2dcbee5fdc041cc7

api: notification targets: fix permission check for POST/PUT
This fixes the error:
  unknown permission test at /usr/share/perl5/PVE/RPCEnvironment.pm line 536. (500)
which occurred when trying to create or update a notification target.

The cause was a permission 'check' parameter for the API handlers
which was nested one level too deep by accident.

This regression was introduced in a previous commit which raised the
needed permissions for notification target management. It likely went
unnoticed because the permission check is skipped for root@pam, so the
error occurs only if using another user.

Reported in the community forum:
https://forum.proxmox.com/threads/158101

Fixes: a3fe9c54 ("api: notifications: require powerful privileges for target management")
Signed-off-by: Lukas Wagner <l.wagner@proxmox.com>
 [ TL: minor commit message touch ups ]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2/Cluster/Notifications.pm
@@ -500,13 +500,11 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new sendmail endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -556,13 +554,11 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing sendmail endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -780,13 +776,11 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new gotify endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -832,13 +826,11 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing gotify endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -1099,13 +1091,11 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new smtp endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -1165,13 +1155,11 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing smtp endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -1423,13 +1411,11 @@ __PACKAGE__->register_method ({
     method => 'POST',
     description => 'Create a new webhook endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },
@@ -1464,13 +1450,11 @@ __PACKAGE__->register_method ({
     method => 'PUT',
     description => 'Update existing webhook endpoint',
     permissions => {
-	check => [
-	    ['and',
-		['perm', '/mapping/notifications', ['Mapping.Modify']],
-		['or',
-		    ['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
-		    ['perm', '/', [ 'Sys.AccessNetwork' ]],
-		],
+	check => ['and',
+	    ['perm', '/mapping/notifications', ['Mapping.Modify']],
+	    ['or',
+		['perm', '/', [ 'Sys.Audit', 'Sys.Modify' ]],
+		['perm', '/', [ 'Sys.AccessNetwork' ]],
 	    ],
 	],
     },