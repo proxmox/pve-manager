proxmox/pve-manager

commit 80da5bb97dbd6a03173365f98c796d96c94f5e85

ui: qemu: add clipboard ComboBox as a advanced option in DisplayEdit
For SPICE and VNC, a different message is displayed.

The backend code for the clipboard option can be found in the
'config: enable vnc clipboard parameter in vga_fmt'-commit in qemu-server.

Signed-off-by: Markus Frank <m.frank@proxmox.com>

www/manager6/qemu/DisplayEdit.js
@@ -35,6 +35,9 @@ Ext.define('PVE.qemu.DisplayInputPanel', {
 		    return Proxmox.Utils.defaultText;
 		}
 	    },
+	    isVNC: get => get('clipboard') === 'vnc',
+	    hideDefaultHint: get => get('isVNC') || get('matchNonGUIOption'),
+	    hideVNCHint: get => !get('isVNC') || get('matchNonGUIOption'),
 	},
     },
 
@@ -71,6 +74,44 @@ Ext.define('PVE.qemu.DisplayInputPanel', {
 	    disabled: '{matchNonGUIOption}',
 	},
     }],
+
+    advancedItems: [
+	{
+	    xtype: 'proxmoxKVComboBox',
+	    name: 'clipboard',
+	    deleteEmpty: false,
+	    value: '__default__',
+	    fieldLabel: gettext('Clipboard'),
+	    comboItems: [
+		['__default__', Proxmox.Utils.defaultText],
+		['vnc', 'VNC'],
+	    ],
+	    bind: {
+		value: '{clipboard}',
+		disabled: '{matchNonGUIOption}',
+	    },
+	},
+	{
+	    xtype: 'displayfield',
+	    name: 'vncHint',
+	    userCls: 'pmx-hint',
+	    value: gettext('You cannot use the default SPICE clipboard if the VNC Clipboard is selected.') + ' ' +
+		gettext('VNC Clipboard requires spice-tools installed in the Guest-VM.'),
+	    bind: {
+		hidden: '{hideVNCHint}',
+	    },
+	},
+	{
+	    xtype: 'displayfield',
+	    name: 'defaultHint',
+	    userCls: 'pmx-hint',
+	    value: gettext('This option depends on your display type.') + ' ' +
+		gettext('If the display type uses SPICE you are able to use the default SPICE Clipboard.'),
+	    bind: {
+		hidden: '{hideDefaultHint}',
+	    },
+	},
+    ],
 });
 
 Ext.define('PVE.qemu.DisplayEdit', {

commit a38c3527a94d29b802531bb8193d4615d8098903

ui: qemu: fix the capitalisation of the word clipboard in DisplayEdit
Fixes: 80da5bb97dbd6a03173365f98c796d96c94f5e85
Signed-off-by: Markus Frank <m.frank@proxmox.com>

www/manager6/qemu/DisplayEdit.js
@@ -95,8 +95,8 @@ Ext.define('PVE.qemu.DisplayInputPanel', {
 	    xtype: 'displayfield',
 	    name: 'vncHint',
 	    userCls: 'pmx-hint',
-	    value: gettext('You cannot use the default SPICE clipboard if the VNC Clipboard is selected.') + ' ' +
-		gettext('VNC Clipboard requires spice-tools installed in the Guest-VM.'),
+	    value: gettext('You cannot use the default SPICE clipboard if the VNC clipboard is selected.') + ' ' +
+		gettext('VNC clipboard requires spice-tools installed in the Guest-VM.'),
 	    bind: {
 		hidden: '{hideVNCHint}',
 	    },
@@ -115,7 +115,7 @@ Ext.define('PVE.qemu.DisplayInputPanel', {
 	    name: 'defaultHint',
 	    userCls: 'pmx-hint',
 	    value: gettext('This option depends on your display type.') + ' ' +
-		gettext('If the display type uses SPICE you are able to use the default SPICE Clipboard.'),
+		gettext('If the display type uses SPICE you are able to use the default SPICE clipboard.'),
 	    bind: {
 		hidden: '{hideDefaultHint}',
 	    },