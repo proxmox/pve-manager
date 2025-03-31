proxmox/pve-manager

commit 52f67025033de0397c1e562a354708fe114897bd

api subscription: output generic apt-auth stanza for ceph
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2/Subscription.pm
@@ -103,6 +103,9 @@ sub write_etc_subscription {
 	    $ceph_auth .= "machine enterprise.proxmox.com/debian/ceph-${ceph_release}"
 	    ." login $info->{key} password $info->{serverid}\n"
 	}
+	# add a generic one to handle the case where a new ceph release was made available while
+	# the subscription info was not yet updated, and as per APT_AUTH.CONF(5) start-with matches.
+	$ceph_auth .= "machine enterprise.proxmox.com/debian/ceph login $info->{key} password $info->{serverid}\n"
 	PVE::Tools::file_set_contents("/etc/apt/auth.conf.d/ceph.conf", $ceph_auth);
     }
 }

commit 3d23bdefe68521116bba1378f6624d7bfa96e759

api: subscription: add missing semicolon
Fixes a build failure.

Fixes: 52f670250 ("api subscription: output generic apt-auth stanza for ceph")
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>

PVE/API2/Subscription.pm
@@ -105,7 +105,7 @@ sub write_etc_subscription {
 	}
 	# add a generic one to handle the case where a new ceph release was made available while
 	# the subscription info was not yet updated, and as per APT_AUTH.CONF(5) start-with matches.
-	$ceph_auth .= "machine enterprise.proxmox.com/debian/ceph login $info->{key} password $info->{serverid}\n"
+	$ceph_auth .= "machine enterprise.proxmox.com/debian/ceph login $info->{key} password $info->{serverid}\n";
 	PVE::Tools::file_set_contents("/etc/apt/auth.conf.d/ceph.conf", $ceph_auth);
     }
 }