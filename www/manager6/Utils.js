proxmox/pve-manager

commit 3f824603043ffb5e9611ebb86346a5fdd9036ff9

ui: backup: also check for backup subtype to classify archive
In anticipation of future storage plugins that might not have
PBS-specific formats or adhere to the vzdump naming scheme for
backups.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Tested-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
Reviewed-by: Wolfgang Bumiller <w.bumiller@proxmox.com>
Link: https://lore.proxmox.com/20250404133204.239783-29-f.ebner@proxmox.com

www/manager6/Utils.js
@@ -694,12 +694,14 @@ Ext.define('PVE.Utils', {
 	'import': gettext('Import'),
     },
 
-    volume_is_qemu_backup: function(volid, format) {
-	return format === 'pbs-vm' || volid.match(':backup/vzdump-qemu-');
+    volume_is_qemu_backup: function(volume) {
+	return volume.format === 'pbs-vm' || volume.volid.match(':backup/vzdump-qemu-') ||
+	    volume.subtype === 'qemu';
     },
 
-    volume_is_lxc_backup: function(volid, format) {
-	return format === 'pbs-ct' || volid.match(':backup/vzdump-(lxc|openvz)-');
+    volume_is_lxc_backup: function(volume) {
+	return volume.format === 'pbs-ct' || volume.volid.match(':backup/vzdump-(lxc|openvz)-') ||
+	    volume.subtype === 'lxc';
     },
 
     authSchema: {

www/manager6/grid/BackupView.js
@@ -29,11 +29,11 @@ Ext.define('PVE.grid.BackupView', {
 	var vmtypeFilter;
 	if (vmtype === 'lxc' || vmtype === 'openvz') {
 	    vmtypeFilter = function(item) {
-		return PVE.Utils.volume_is_lxc_backup(item.data.volid, item.data.format);
+		return PVE.Utils.volume_is_lxc_backup(item.data);
 	    };
 	} else if (vmtype === 'qemu') {
 	    vmtypeFilter = function(item) {
-		return PVE.Utils.volume_is_qemu_backup(item.data.volid, item.data.format);
+		return PVE.Utils.volume_is_qemu_backup(item.data);
 	    };
 	} else {
 	    throw "unsupported VM type '" + vmtype + "'";

www/manager6/storage/BackupView.js
@@ -86,9 +86,9 @@ Ext.define('PVE.storage.BackupView', {
 		disabled: true,
 		handler: function(b, e, rec) {
 		    let vmtype;
-		    if (PVE.Utils.volume_is_qemu_backup(rec.data.volid, rec.data.format)) {
+		    if (PVE.Utils.volume_is_qemu_backup(rec.data)) {
 			vmtype = 'qemu';
-		    } else if (PVE.Utils.volume_is_lxc_backup(rec.data.volid, rec.data.format)) {
+		    } else if (PVE.Utils.volume_is_lxc_backup(rec.data)) {
 			vmtype = 'lxc';
 		    } else {
 			return;

commit f8087e0ff077984db7d4c9e9707558213444532a

ui: fix regression with checking if volume is QEMU backup
The function was changed but not all call sites where adapted,
resulting in a JavaScript exception when, e.g., trying to open the
file-restore dialogue for a backup from PBS storage.

Make the check function generic and support passing either a full
volume info object or the volume ID and the volume format as two
separate string parameters.

Fixes: 3f8246030 ("ui: backup: also check for backup subtype to classify archive")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/Utils.js
@@ -694,9 +694,18 @@ Ext.define('PVE.Utils', {
 	'import': gettext('Import'),
     },
 
-    volume_is_qemu_backup: function(volume) {
-	return volume.format === 'pbs-vm' || volume.volid.match(':backup/vzdump-qemu-') ||
-	    volume.subtype === 'qemu';
+     // volume can be a full volume info object, in which case the format parameter is ignored, or
+     // you can pass the volume ID and format as separate string parameters.
+    volume_is_qemu_backup: function(volume, format) {
+	let volid, subtype;
+	if (typeof volume === 'string') {
+	    volid = volume;
+	} else if (typeof volume === 'object') {
+	    ({ volid, format, subtype } = volume);
+	} else {
+	    console.error("internal error - unexpected type", volume);
+	}
+	return format === 'pbs-vm' || volid.match(':backup/vzdump-qemu-') || subtype === 'qemu';
     },
 
     volume_is_lxc_backup: function(volume) {