b5943584129cb3283e1f25a65f4d982f037e40d1
Christoph Heiss <c.heiss@proxmox.com>
2025-07-07 11:14:09 +0200

    ui: panel: TagConfig: link to more specific online help section
    This section is available since pve-docs 8.2.4, released November 2024.
    Includes the necessary bump to accommodate that.
    
    Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
    Link: https://lore.proxmox.com/20250707091412.314631-4-c.heiss@proxmox.com

--- www/manager6/panel/TagConfig.js
+++ www/manager6/panel/TagConfig.js
@@ -2,6 +2,5 @@ Ext.define('PVE.panel.TagConfig', {
     extend: 'PVE.panel.Config',
     alias: 'widget.pveTagConfig',
 
-    //onlineHelp: 'gui_tags', // TODO: use this one once available
-    onlineHelp: 'chapter_gui',
+    onlineHelp: 'gui_tags',
 });
