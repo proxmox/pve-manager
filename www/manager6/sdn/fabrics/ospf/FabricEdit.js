e3e3ae51afb142b033b41a9c90bcffcb282dfc6a
Stefan Hanreich <s.hanreich@proxmox.com>
2025-07-16 15:08:28 +0200

    fabric: add OSPF fabric edit panel
    Extends the common FabricEdit component and adds the OSPF-specific
    items to it.
    
    Co-authored-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Signed-off-by: Gabriel Goller <g.goller@proxmox.com>
    Link: https://lore.proxmox.com/20250716130837.585796-68-g.goller@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ www/manager6/sdn/fabrics/ospf/FabricEdit.js
@@ -0,0 +1,20 @@
+Ext.define('PVE.sdn.Fabric.Ospf.Fabric.Edit', {
+    extend: 'PVE.sdn.Fabric.Fabric.Edit',
+
+    subject: 'OSPF',
+    onlineHelp: 'pvesdn_ospf_fabric',
+
+    extraRequestParams: {
+        protocol: 'ospf',
+    },
+
+    additionalItems: [
+        {
+            xtype: 'textfield',
+            fieldLabel: gettext('Area'),
+            labelWidth: 120,
+            name: 'area',
+            allowBlank: false,
+        },
+    ],
+});
