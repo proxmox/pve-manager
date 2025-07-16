adc909717741e1c9f0c39000c45fb65e8338ad56
Stefan Hanreich <s.hanreich@proxmox.com>
2025-07-16 15:08:26 +0200

    fabric: add generic fabric edit panel
    Add generic base component to add and edit Fabrics, which contains the
    fields required for every protocol. The properties for every protocol
    are stored in different components and each extend this one.
    
    Co-authored-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Signed-off-by: Gabriel Goller <g.goller@proxmox.com>
    Link: https://lore.proxmox.com/20250716130837.585796-66-g.goller@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ www/manager6/sdn/fabrics/FabricEdit.js
@@ -0,0 +1,57 @@
+Ext.define('PVE.sdn.Fabric.Fabric.Edit', {
+    extend: 'Proxmox.window.Edit',
+    mixins: ['Proxmox.Mixin.CBind'],
+
+    fabricId: undefined,
+    baseUrl: '/cluster/sdn/fabrics/fabric',
+
+    items: [
+        {
+            xtype: 'textfield',
+            name: 'digest',
+            hidden: true,
+            allowBlank: true,
+        },
+        {
+            xtype: 'proxmoxtextfield',
+            fieldLabel: gettext('Name'),
+            labelWidth: 120,
+            name: 'id',
+            cbind: {
+                disabled: '{!isCreate}',
+            },
+        },
+        {
+            xtype: 'proxmoxtextfield',
+            fieldLabel: gettext('IPv4 Prefix'),
+            labelWidth: 120,
+            name: 'ip_prefix',
+            allowBlank: true,
+            skipEmptyText: true,
+            cbind: {
+                disabled: '{!isCreate}',
+                deleteEmpty: '{!isCreate}',
+            },
+        },
+    ],
+
+    additionalItems: [],
+
+    initComponent: function () {
+        let me = this;
+
+        me.isCreate = me.fabricId === undefined;
+        me.autoLoad = !me.isCreate;
+        me.method = me.isCreate ? 'POST' : 'PUT';
+
+        if (!me.isCreate) {
+            me.url = `${me.baseUrl}/${me.fabricId}`;
+        } else {
+            me.url = me.baseUrl;
+        }
+
+        me.items.push(...me.additionalItems);
+
+        me.callParent();
+    },
+});
