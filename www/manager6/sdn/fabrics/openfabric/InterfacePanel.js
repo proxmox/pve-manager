32f74c93718c3d1235064bd5b05e2197ca339a74
Stefan Hanreich <s.hanreich@proxmox.com>
2025-07-16 15:08:21 +0200

    fabric: add OpenFabric interface properties
    This component extends the InterfacePanel and adds Openfabric specific
    form fields. Hello Multiplier is hidden by default, but can be
    activated in the column settings of the DataGrid.
    
    Co-authored-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Signed-off-by: Gabriel Goller <g.goller@proxmox.com>
    Link: https://lore.proxmox.com/20250716130837.585796-61-g.goller@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ www/manager6/sdn/fabrics/openfabric/InterfacePanel.js
@@ -0,0 +1,33 @@
+Ext.define('PVE.sdn.Fabric.OpenFabric.InterfacePanel', {
+    extend: 'PVE.sdn.Fabric.InterfacePanel',
+
+    additionalColumns: [
+        {
+            text: gettext('IPv6'),
+            xtype: 'widgetcolumn',
+            dataIndex: 'ip6',
+            flex: 1,
+            widget: {
+                xtype: 'proxmoxtextfield',
+                isFormField: false,
+                bind: {
+                    disabled: '{record.isDisabled}',
+                },
+            },
+        },
+        {
+            text: gettext('Hello Multiplier'),
+            xtype: 'widgetcolumn',
+            dataIndex: 'hello_multiplier',
+            flex: 1,
+            hidden: true,
+            widget: {
+                xtype: 'proxmoxintegerfield',
+                isFormField: false,
+                bind: {
+                    disabled: '{record.isDisabled}',
+                },
+            },
+        },
+    ],
+});
