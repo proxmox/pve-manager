81bf0378f1b6d6278b2a9f1a7beb8ca917e17e9b
Stefan Hanreich <s.hanreich@proxmox.com>
2025-07-16 15:08:27 +0200

    fabric: add OpenFabric fabric edit panel
    Add a component that extends the common FabricEdit component and adds
    the OpenFabric-specific items to it. Those are currently the Hello
    Interval and CSNP interval, which can be configured globally for all
    members of the fabric.
    
    Since OSPF currently does not provide IPv6 support (yet), we also move
    the IPv6 prefix to the Openfabric edit panel, to avoid showing the
    IPv6 prefix input field in the OSPF fabric edit panel.
    
    As we don't enable IPv6 forwarding globally if a IPv6 fabric is created, show
    a big warning that a use has to enable it manually.
    
    Co-authored-by: Stefan Hanreich <s.hanreich@proxmox.com>
    Signed-off-by: Gabriel Goller <g.goller@proxmox.com>
    Link: https://lore.proxmox.com/20250716130837.585796-67-g.goller@proxmox.com
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

---
+++ www/manager6/sdn/fabrics/openfabric/FabricEdit.js
@@ -0,0 +1,67 @@
+Ext.define('PVE.sdn.Fabric.OpenFabric.Fabric.Edit', {
+    extend: 'PVE.sdn.Fabric.Fabric.Edit',
+
+    subject: 'OpenFabric',
+    onlineHelp: 'pvesdn_openfabric_fabric',
+
+    viewModel: {
+        data: {
+            showIpv6ForwardingHint: false,
+        },
+    },
+
+    extraRequestParams: {
+        protocol: 'openfabric',
+    },
+
+    additionalItems: [
+        {
+            xtype: 'displayfield',
+            value: 'To make IPv6 fabrics work, enable global IPv6 forwarding on all nodes. Click on the Help button for more details.',
+            bind: {
+                hidden: '{!showIpv6ForwardingHint}',
+            },
+            userCls: 'pmx-hint',
+        },
+        {
+            xtype: 'proxmoxtextfield',
+            fieldLabel: gettext('IPv6 Prefix'),
+            labelWidth: 120,
+            name: 'ip6_prefix',
+            allowBlank: true,
+            skipEmptyText: true,
+            cbind: {
+                disabled: '{!isCreate}',
+                deleteEmpty: '{!isCreate}',
+            },
+            listeners: {
+                change: function (textbox, value) {
+                    let vm = textbox.up('window').getViewModel();
+                    vm.set('showIpv6ForwardingHint', !!value);
+                },
+            },
+        },
+        {
+            xtype: 'proxmoxintegerfield',
+            fieldLabel: gettext('Hello Interval'),
+            labelWidth: 120,
+            name: 'hello_interval',
+            allowBlank: true,
+            skipEmptyText: true,
+            cbind: {
+                deleteEmpty: '{!isCreate}',
+            },
+        },
+        {
+            xtype: 'proxmoxintegerfield',
+            fieldLabel: gettext('CSNP Interval'),
+            labelWidth: 120,
+            name: 'csnp_interval',
+            allowBlank: true,
+            skipEmptyText: true,
+            cbind: {
+                deleteEmpty: '{!isCreate}',
+            },
+        },
+    ],
+});
