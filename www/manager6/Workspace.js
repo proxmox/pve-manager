6738b721f8f010795aecfcda87327ab39af55f51
Thomas Lamprecht <t.lamprecht@proxmox.com>
2025-07-18 15:39:14 +0200

    ui: add beta text with link to bugtracker
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/Workspace.js
+++ www/manager6/Workspace.js
@@ -344,6 +344,10 @@ Ext.define('PVE.StdWorkspace', {
                             },
                         },
                         {
+                            padding: 5,
+                            html: '<a href="https://bugzilla.proxmox.com" target="_blank">BETA</a>',
+                        },
+                        {
                             xtype: 'pveGlobalSearchField',
                             tree: rtree,
                         },
