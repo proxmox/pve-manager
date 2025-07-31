57a09b20414c482f1c8f84954d66b8dd65c348fe
Thomas Lamprecht <t.lamprecht@proxmox.com>
2025-07-31 11:42:14 +0200

    ui: use title-case for affinity selections
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- www/manager6/ha/rules/ResourceAffinityRuleEdit.js
+++ www/manager6/ha/rules/ResourceAffinityRuleEdit.js
@@ -13,8 +13,8 @@ Ext.define('PVE.ha.rules.ResourceAffinityInputPanel', {
                 fieldLabel: gettext('Affinity'),
                 allowBlank: false,
                 comboItems: [
-                    ['positive', gettext('Keep together')],
-                    ['negative', gettext('Keep separate')],
+                    ['positive', gettext('Keep Together')],
+                    ['negative', gettext('Keep Separate')],
                 ],
             },
         ];
