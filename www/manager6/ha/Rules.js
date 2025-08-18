proxmox/pve-manager

commit fb289b0d30512ed3a039718e7412d116e13656da

ha: affinity rules: make edit/remove button declrative configs
No benefit in having this pre-created and assigned in a variable, in
our more modern components we normally try to have as much as possible
defined declaratively, and ideally avoid having an initComponent
completely–or at least keep it to the required minimum.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/ha/Rules.js
@@ -77,19 +77,6 @@ Ext.define('PVE.ha.RulesBaseView', {
             createRuleEditWindow(rule);
         };
 
-        let editButton = Ext.create('Proxmox.button.Button', {
-            text: gettext('Edit'),
-            disabled: true,
-            selModel: sm,
-            handler: runEditor,
-        });
-
-        let removeButton = Ext.create('Proxmox.button.StdRemoveButton', {
-            selModel: sm,
-            baseurl: '/cluster/ha/rules/',
-            callback: reloadStore,
-        });
-
         Ext.apply(me, {
             selModel: sm,
             viewConfig: {
@@ -101,8 +88,19 @@ Ext.define('PVE.ha.RulesBaseView', {
                     text: gettext('Add'),
                     handler: () => createRuleEditWindow(),
                 },
-                editButton,
-                removeButton,
+                {
+                    xtype: 'button',
+                    text: gettext('Edit'),
+                    disabled: true,
+                    selModel: sm,
+                    handler: runEditor,
+                },
+                {
+                    xtype: 'proxmoxStdRemoveButton',
+                    selModel: sm,
+                    baseurl: '/cluster/ha/rules/',
+                    callback: reloadStore,
+                },
             ],
             listeners: {
                 activate: reloadStore,

commit 8e84f7d897b9b3291c3e611783080bd35b8a052a

ui: ha: rules: fix edit button
As reported in the community forum [0], the 'Edit' button would stay
disabled. Use a proxmoxButton to fix it, which installs a monitor for
selectionchange.

[0]: https://forum.proxmox.com/threads/169258/post-792029

Fixes: fb289b0d ("ha: affinity rules: make edit/remove button declrative configs")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

www/manager6/ha/Rules.js
@@ -96,7 +96,7 @@ Ext.define('PVE.ha.RulesBaseView', {
                     handler: () => createRuleEditWindow(),
                 },
                 {
-                    xtype: 'button',
+                    xtype: 'proxmoxButton',
                     text: gettext('Edit'),
                     disabled: true,
                     selModel: sm,