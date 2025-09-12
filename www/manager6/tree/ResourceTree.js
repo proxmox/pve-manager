proxmox/pve-manager

commit d2660fc7a4b5add154e5694dc40a188d9403ab47

ui: resource tree: improve performance on initial update
When we insert nodes into the tree, we use 'insertBefore' of extjs'
NodeInterface. When the node is inside a TreeStore, it calls
'registerNode' to handle some events and accounting. Sadly it does so
not only for the inserted node, but also for the node in which is
inserted too and that calls 'registerNode' again for all of its
children.

So inserting a large number of guests under node this way has (at least)
O(n^2) calls to registerNode.

To workaround this, create the first tree node structure outside the
TreeStore and add it at the end. Further insertions are more likely to
only come in small numbers. (Still have to look into if we can avoid
that behavior there too)

This improves the time spend in 'registerNode' (in my ~10000 guests test
setup) from 4,081.6 ms to about 2.7ms.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20250905120627.2585826-4-d.csapak@proxmox.com

www/manager6/tree/ResourceTree.js
@@ -328,10 +328,22 @@ Ext.define('PVE.tree.ResourceTree', {
             return node;
         };
 
+        let firstUpdate = true;
+
         let updateTree = function () {
             store.suspendEvents();
 
-            let rootnode = me.store.getRootNode();
+            let rootnode;
+            if (firstUpdate) {
+                rootnode = Ext.create('Ext.data.TreeModel', {
+                    expanded: true,
+                    id: 'root',
+                    text: gettext('Datacenter'),
+                    iconCls: 'fa fa-server',
+                });
+            } else {
+                rootnode = me.store.getRootNode();
+            }
             // remember selected node (and all parents)
             let sm = me.getSelectionModel();
             let lastsel = sm.getSelection()[0];
@@ -449,6 +461,11 @@ Ext.define('PVE.tree.ResourceTree', {
                 me.selectById(lastsel.data.id);
             } else if (lastsel && reselect) {
                 me.selectById(lastsel.data.id);
+            }
+
+            if (firstUpdate) {
+                me.store.setRoot(rootnode);
+                firstUpdate = false;
             }
 
             // on first tree load set the selection from the stateful provider

commit dbc0ca81d68bc8114d18dd3edd0ac4bb1ca8cac6

ui: fix datacenter notes toolbar visibility
the recent commit
 d2660fc7 (ui: resource tree: improve performance on initial update)
changed how we construct the resource tree, namely outside the
treestore. While it worked fine mostly, the standard
`Ext.data.TreeModel` was used. This lead to problem with the detection
of some things, since we expected all properties that were defined on
the custom `PVETree` model.

To fix this, create an instance of `PVETree` instead.
Note that this might also fix other things that depend on the
PVETree specific properties on the datacenter root node.

Fixes: d2660fc7 (ui: resource tree: improve performance on initial update)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Tested-by: Michael Köppl <m.koeppl@proxmox.com>
Link: https://lore.proxmox.com/20250912113242.3139402-1-d.csapak@proxmox.com

www/manager6/tree/ResourceTree.js
@@ -339,7 +339,7 @@ Ext.define('PVE.tree.ResourceTree', {
 
             let rootnode;
             if (firstUpdate) {
-                rootnode = Ext.create('Ext.data.TreeModel', {
+                rootnode = Ext.create('PVETree', {
                     expanded: true,
                     id: 'root',
                     text: gettext('Datacenter'),