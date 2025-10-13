proxmox/pve-manager

commit e19de81f5469eb2e4e45296ebeef16754abacc7a

ui: resource tree: fix change detection
when trying to detect changes of resources, we compare a list of
properties of the existing nodes in the tree with the ones we got from
the api call, so that we can update only those that changed.

One of these properties is the 'text' one, which is calculated from e.g.
the vmid and name (or the name and host, depending on the type).

Sadly, when inserting/updating the node, we modified the text property
in every case, at least adding a '<span></span>' around the existing
text. This meant that every resource was updated every time instead of
only when something changed.

To fix this, remote the 'text' property from the to checked ones, and
add all the properties that are used to compile the text one.

This reduces the time of updateTree in my test-setup (~10000 guests)
when nothing changed from ~100ms to ~15ms and reduces scroll stutter
during such an update.

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20250905120627.2585826-5-d.csapak@proxmox.com

www/manager6/tree/ResourceTree.js
@@ -291,7 +291,11 @@ Ext.define('PVE.tree.ResourceTree', {
         let stateid = 'rid';
 
         const changedFields = [
-            'text',
+            'disk',
+            'maxdisk',
+            'vmid',
+            'name',
+            'type',
             'running',
             'template',
             'status',

commit 952a4f615731c3de0351c94b83bd97340c7e7798

fix #6815: ui: resource tree: update tag colors properly
In commit:
 e19de81f (ui: resource tree: fix change detection)
the change detection for resource tree nodes was fixed so that it did
not update on every refresh anymore.

This now also means that when the ui options are loaded (which contains
the tag color overrides) we now have to correctly update nodes which
contains tags, otherwise the standard colors will be shown.

There is already an event handler for the 'loadedUiOptions', so simply
adapt it to also update the tree nodes that contains tags.

Fixes: e19de81f (ui: resource tree: fix change detection)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20251010072802.798062-1-d.csapak@proxmox.com

www/manager6/tree/ResourceTree.js
@@ -605,7 +605,7 @@ Ext.define('PVE.tree.ResourceTree', {
         me.mon(Ext.GlobalEvents, 'loadedUiOptions', () => {
             me.store.getRootNode().cascadeBy({
                 before: function (node) {
-                    if (node.data.groupbyid) {
+                    if (node.data.groupbyid || node.data.tags) {
                         node.beginEdit();
                         let info = node.data;
                         me.setIconCls(info);