6e551f05438039c1d99bb8755a681a235f46b936
Gabriel Goller <g.goller@proxmox.com>
2026-05-05 11:13:38 +0200

    fabrics: ospf: add network-type property in interface panel
    This allows the user to select a specific network type which will be
    used for the ospf interface.
    
    Signed-off-by: Gabriel Goller <g.goller@proxmox.com>

--- www/manager6/sdn/fabrics/ospf/InterfacePanel.js
+++ www/manager6/sdn/fabrics/ospf/InterfacePanel.js
@@ -1,5 +1,54 @@
+Ext.define('PVE.sdn.Fabric.Ospf.NetworkTypeSelector', {
+    extend: 'Proxmox.form.ComboGrid',
+    alias: ['widget.pveOspfNetworkTypeSelector'],
+
+    valueField: 'name',
+    displayField: 'name',
+
+    emptyText: 'auto',
+
+    listConfig: {
+        columns: [
+            {
+                header: gettext('NetworkType'),
+                dataIndex: 'name',
+                hideable: false,
+                sortable: true,
+                flex: 3,
+            },
+        ],
+        width: 360,
+    },
+
+    store: {
+        fields: ['name'],
+        data: [
+            {name: 'broadcast'},
+            {name: 'non-broadcast'},
+            {name: 'point-to-multipoint'},
+            {name: 'point-to-point'},
+        ],
+    },
+});
+
 Ext.define('PVE.sdn.Fabric.Ospf.InterfacePanel', {
     extend: 'PVE.sdn.Fabric.InterfacePanel',
 
     hasIpv6Support: false,
+
+    additionalColumns: [
+        {
+            text: gettext('Network Type'),
+            xtype: 'widgetcolumn',
+            dataIndex: 'network_type',
+            flex: 1,
+            widget: {
+                xtype: 'pveOspfNetworkTypeSelector',
+                isFormField: false,
+                bind: {
+                    disabled: '{record.isDisabled}',
+                },
+            },
+        },
+    ],
 });
