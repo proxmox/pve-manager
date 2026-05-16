proxmox/pve-manager

commit cdc488adf70495627b9fc1788abcc58ffcc73e60

network: sdn: generate wireguard configuration on apply
Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20260512173145.596958-17-s.hanreich@proxmox.com
 [TL: pass the apply flag to generate_wireguard_config so wg(8) gets
  syncconf'd here as well, the ifupdown2 post-up only covers the
  fresh-interface case so edits to existing peer sets stay desynced
  otherwise; require the WireGuard module explicitly instead of
  relying on a transitive load via the API tree]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2/Network.pm
@@ -20,6 +20,7 @@ use base qw(PVE::RESTHandler);
 my $have_sdn;
 eval {
     require PVE::Network::SDN;
+    require PVE::Network::SDN::WireGuard;
     $have_sdn = 1;
 };
 
@@ -922,6 +923,7 @@ __PACKAGE__->register_method({
             if ($have_sdn) {
                 PVE::Network::SDN::generate_etc_network_config();
                 PVE::Network::SDN::generate_dhcp_config();
+                PVE::Network::SDN::WireGuard::generate_wireguard_config(1);
             }
 
             my $err = sub {

commit 51056af5b42e46836909e4950e1a694f9441026f

network: drop apply flag when generating wireguard config
Otherwise, this produces an error when applying a WireGuard
configuration with newly created interfaces, because `wg syncconf`
gets called for a non-existing interface. post-up commands get
executed for existing interfaces anyway, so subsequent calls to
ifreload for existing interfaces run `wg syncconf`, making separately
applying here unnecessary.

Fixes: cdc488adf70495627b9fc1788abcc58ffcc73e60
Signed-off-by: Stefan Hanreich <s.hanreich@proxmox.com>
Link: https://lore.proxmox.com/20260515145622.419236-1-s.hanreich@proxmox.com

PVE/API2/Network.pm
@@ -923,7 +923,7 @@ __PACKAGE__->register_method({
             if ($have_sdn) {
                 PVE::Network::SDN::generate_etc_network_config();
                 PVE::Network::SDN::generate_dhcp_config();
-                PVE::Network::SDN::WireGuard::generate_wireguard_config(1);
+                PVE::Network::SDN::WireGuard::generate_wireguard_config();
             }
 
             my $err = sub {