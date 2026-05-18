proxmox/pve-manager

commit 6be73bfec6cbbec6a15a71a7f450f96aa040ae48

ui: qemu: make sortByPreviousUsage architecture aware
Different architectures may have different controller priorities,
so this has to use the architecture specific defaults.

For this, we have to pass through the nodename sometimes (to get the
host architecture).

The other notable change is that in the multi hd edit panel, we now only
add the first disk in the 'afterrender' event. This is done so that we
guaranteed have a nodename, which is not the case when called in 'init'.
(The 'bind' mechanism we use in the wizard changes that only after the
'init' functions are called.)

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20260302132913.3169037-13-d.csapak@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/panel/MultiDiskEdit.js
@@ -5,6 +5,7 @@ Ext.define('PVE.panel.MultiDiskPanel', {
 
     setNodename: function (nodename) {
         this.items.each((panel) => panel.setNodename(nodename));
+        this.nodename = nodename;
     },
 
     border: false,
@@ -210,12 +211,13 @@ Ext.define('PVE.panel.MultiDiskPanel', {
             'grid[reference=grid]': {
                 selectionchange: 'onSelectionChange',
             },
-        },
-
-        init: function (view) {
-            let me = this;
-            me.onAdd();
-            me.lookup('grid').getSelectionModel().select(0, false);
+            '#': {
+                afterrender: function (view) {
+                    let me = this;
+                    me.onAdd();
+                    me.lookup('grid').getSelectionModel().select(0, false);
+                },
+            },
         },
     },
 

www/manager6/Utils.js
@@ -1962,10 +1962,8 @@ Ext.define('PVE.Utils', {
             return true;
         },
 
-        sortByPreviousUsage: function (vmconfig, controllerList) {
-            if (!controllerList) {
-                controllerList = ['ide', 'virtio', 'scsi', 'sata'];
-            }
+        sortByPreviousUsage: function (vmconfig, nodename) {
+            let controllerList = ['ide', 'virtio', 'scsi', 'sata'];
             let usedControllers = {};
             for (const type of Object.keys(PVE.Utils.diskControllerMaxIDs)) {
                 usedControllers[type] = 0;
@@ -1981,7 +1979,8 @@ Ext.define('PVE.Utils', {
                 }
             }
 
-            let sortPriority = PVE.qemu.OSDefaults.getDefaults(vmconfig.ostype).busPriority;
+            let arch = PVE.qemu.Architecture.getGuestArchitecture(vmconfig.arch, nodename);
+            let sortPriority = PVE.qemu.OSDefaults.getDefaults(vmconfig.ostype, arch).busPriority;
 
             let sortedList = Ext.clone(controllerList);
             sortedList.sort(function (a, b) {

www/manager6/form/ControllerSelector.js
@@ -7,6 +7,13 @@ Ext.define('PVE.form.ControllerSelector', {
 
     vmconfig: {}, // used to check for existing devices
 
+    nodename: undefined,
+
+    setNodename: function (nodename) {
+        let me = this;
+        me.nodename = nodename;
+    },
+
     setToFree: function (controllers, busField, deviceIDField) {
         let me = this;
         let freeId = PVE.Utils.nextFreeDisk(controllers, me.vmconfig);
@@ -42,7 +49,7 @@ Ext.define('PVE.form.ControllerSelector', {
             clist = ['ide', 'scsi', 'sata'];
         } else {
             // in most cases we want to add a disk to the same controller we previously used
-            clist = PVE.Utils.sortByPreviousUsage(me.vmconfig);
+            clist = PVE.Utils.sortByPreviousUsage(me.vmconfig, me.nodename);
         }
 
         me.setToFree(clist, bussel, deviceid);

www/manager6/qemu/CIDriveEdit.js
@@ -20,6 +20,7 @@ Ext.define('PVE.qemu.CIDriveInputPanel', {
     setNodename: function (nodename) {
         var me = this;
         me.down('#hdstorage').setNodename(nodename);
+        me.down('#drive').setNodename(nodename);
         me.down('#hdimage').setStorage(undefined, nodename);
     },
 
@@ -39,6 +40,7 @@ Ext.define('PVE.qemu.CIDriveInputPanel', {
                 withVirtIO: false,
                 itemId: 'drive',
                 fieldLabel: gettext('CloudInit Drive'),
+                nodename: me.nodename,
                 name: 'drive',
             },
             {

www/manager6/qemu/MultiHDEdit.js
@@ -17,7 +17,8 @@ Ext.define('PVE.qemu.MultiHDPanel', {
             ) - 1,
 
         getNextFreeDisk: function (vmconfig) {
-            let clist = PVE.Utils.sortByPreviousUsage(vmconfig);
+            let nodename = this.getView().nodename;
+            let clist = PVE.Utils.sortByPreviousUsage(vmconfig, nodename);
             return PVE.Utils.nextFreeDisk(clist, vmconfig);
         },
 

www/manager6/qemu/NetworkEdit.js
@@ -264,7 +264,11 @@ Ext.define('PVE.qemu.NetworkEdit', {
                     }
 
                     let ostype = me.vmconfig.ostype;
-                    let defaults = PVE.qemu.OSDefaults.getDefaults(ostype);
+                    let arch = PVE.qemu.Architecture.getGuestArchitecture(
+                        me.vmconfig.arch,
+                        me.nodename,
+                    );
+                    let defaults = PVE.qemu.OSDefaults.getDefaults(ostype, arch);
                     let data = {
                         model: defaults.networkCard,
                     };

www/manager6/window/GuestDiskReassign.js
@@ -171,6 +171,7 @@ Ext.define('PVE.window.GuestDiskReassign', {
             disabled: true,
             cbind: {
                 hidden: '{!isQemu}',
+                nodename: '{nodename}',
             },
         },
         {

commit aecf27bdce984de3516691e5df7e7528b1b3c78d

fix #7599: ui: qemu: wizard: fix disk controller for windows with second cd drive
The MultiDiskPanel recently changed to only add the first disk in
afterrender to make sure we have a nodename to determine the
architecture of the vm (for architecture specific defaults).

In contrast the os panel in the wizard assumed the first disk was
already added and changed the controller value based on e.g. if there is
a second cd drive for windows virtio drivers (set it to scsi in that
case).

For that to work, the disk has to be added sooner. The first available
point in time is when we add set the nodename the first time to a truthy
value, so do it there instead (and save that information to not add a
disk on every setNodename, only the first valid one).

This fixes the issue that the disk controller was still set to IDE in
case the user added a second cd drive for virtio drivers.

Fixes: 6be73bfe (ui: qemu: make sortByPreviousUsage architecture aware)
Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>
Link: https://lore.proxmox.com/20260518082419.862609-1-d.csapak@proxmox.com

www/manager6/panel/MultiDiskEdit.js
@@ -4,9 +4,18 @@ Ext.define('PVE.panel.MultiDiskPanel', {
     mixins: ['Proxmox.Mixin.CBind'],
 
     setNodename: function (nodename) {
-        this.items.each((panel) => panel.setNodename(nodename));
-        this.nodename = nodename;
+        let me = this;
+        me.items.each((panel) => panel.setNodename(nodename));
+        me.nodename = nodename;
+
+        if (!me.addedFirstDisk && !!nodename) {
+            let controller = me.getController();
+            controller.onAdd();
+            me.lookup('grid').getSelectionModel().select(0, false);
+        }
     },
+
+    addedFirstDisk: false,
 
     border: false,
     bodyBorder: false,
@@ -21,7 +30,12 @@ Ext.define('PVE.panel.MultiDiskPanel', {
         vmconfig: {},
 
         onAdd: function () {
-            this.addDiskChecked(false);
+            let me = this;
+            let view = me.getView();
+            me.addDiskChecked(false);
+            if (!view.addedFirstDisk) {
+                view.addedFirstDisk = true;
+            }
         },
 
         onImport: function () {
@@ -210,13 +224,6 @@ Ext.define('PVE.panel.MultiDiskPanel', {
             },
             'grid[reference=grid]': {
                 selectionchange: 'onSelectionChange',
-            },
-            '#': {
-                afterrender: function (view) {
-                    let me = this;
-                    me.onAdd();
-                    me.lookup('grid').getSelectionModel().select(0, false);
-                },
             },
         },
     },