proxmox/pve-manager

commit 7120652d2cb53945a84ebecf66c871762fd57d0f

api: capabilities: register module for VM CPU flags
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20251107144429.126790-6-f.ebner@proxmox.com

PVE/API2/Capabilities.pm
@@ -7,6 +7,7 @@ use PVE::JSONSchema qw(get_standard_option);
 use PVE::RESTHandler;
 
 use PVE::API2::Qemu::CPU;
+use PVE::API2::Qemu::CPUFlags;
 use PVE::API2::Qemu::Machine;
 use PVE::API2::NodeCapabilities::Qemu::Migration;
 
@@ -15,6 +16,11 @@ use base qw(PVE::RESTHandler);
 __PACKAGE__->register_method({
     subclass => "PVE::API2::Qemu::CPU",
     path => 'qemu/cpu',
+});
+
+__PACKAGE__->register_method({
+    subclass => "PVE::API2::Qemu::CPUFlags",
+    path => 'qemu/cpu-flags',
 });
 
 __PACKAGE__->register_method({

commit 909de0dda0b87bdab8a1636a59da525f9276de58

api: capabilities: advertise qemu/cpu-flags subroute in index
The qemu/cpu-flags sub-class was registered in commit 7120652d2 ("api:
capabilities: register module for VM CPU flags") but the hardcoded
child list in qemu_caps_index was not updated, so tools that walk the
index tree (pvesh ls, schema-driven clients) miss it.

Fixes: 7120652d2 ("api: capabilities: register module for VM CPU flags")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/API2/Capabilities.pm
@@ -90,7 +90,10 @@ __PACKAGE__->register_method({
         my ($param) = @_;
 
         my $result = [
-            { name => 'cpu' }, { name => 'machines' }, { name => 'migration' },
+            { name => 'cpu' },
+            { name => 'cpu-flags' },
+            { name => 'machines' },
+            { name => 'migration' },
         ];
 
         return $result;