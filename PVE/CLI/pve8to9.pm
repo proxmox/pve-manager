proxmox/pve-manager

commit d8789083d930bfc85f9ed4d3899593c4c62b303e

pve8to9: avoid logging failure when old kernel metapackages are not installed
Since the $noerr parameter was not passed along, the get_pkg
subroutine would log a failure when it couldn't determine the
installed package version for old kernel metapackages, e.g.:
> FAIL: unable to determine installed pve-kernel-6.5 version.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Tested-by: Shannon Sterz <s.sterz@proxmox.com>
Reviewed-by: Shannon Sterz <s.sterz@proxmox.com>
Link: https://lore.proxmox.com/all/20260519085148.42853-1-f.ebner@proxmox.com
(cherry picked from commit 2385f49c54c452f7676efe8f9296335917db060e)

PVE/CLI/pve8to9.pm
@@ -255,7 +255,7 @@ sub check_pve_packages {
             my $outdated_kernel_meta_pkgs = [];
             for my $kernel_meta_version ('6.2', '6.5', '6.8', '6.11') {
                 my $pkg = "pve-kernel-${kernel_meta_version}";
-                if ($get_pkg->($pkg)) {
+                if ($get_pkg->($pkg, 1)) {
                     push @$outdated_kernel_meta_pkgs, $pkg;
                 }
             }

commit d38a429d06033d58b2231af6f332319a1d26b028

pve8to9: add $noerr to $get_pkg
Fixes: d8789083d930 "pve8to9: avoid logging failure when old kernel metapackages are not installed"

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

PVE/CLI/pve8to9.pm
@@ -162,7 +162,7 @@ my $log_systemd_unit_state = sub {
 
 my $versions;
 my $get_pkg = sub {
-    my ($pkg) = @_;
+    my ($pkg, $noerr) = @_;
 
     $versions = eval { PVE::API2::APT->versions({ node => $nodename }) } if !defined($versions);
 
@@ -175,7 +175,7 @@ my $get_pkg = sub {
 
     my $pkgs = [grep { $_->{Package} eq $pkg } @$versions];
     if (!defined $pkgs || $pkgs == 0) {
-        log_fail("unable to determine installed $pkg version.");
+        log_fail("unable to determine installed $pkg version.") if !$noerr;
         return undef;
     } else {
         return $pkgs->[0];