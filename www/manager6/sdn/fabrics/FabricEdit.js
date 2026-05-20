proxmox/pve-manager

commit e5e3374273af56dad849ab970d057cc7f4338dfd

ui: fabrics: edit: make ipv4/6 support generic over fabric panels
This allows selectively enabling IPv4/6 in the child components,
without having to re-define the respective fields in every child
component. A later commit will introduce the Wireguard fabric, which
will have neither a IPv4 nor IPv6 prefix for now, so the option to
exclude those fields will be used there as well.

Co-authored-by: Stefan Hanreich <s.hanreich@proxmox.com>
Signed-off-by: Christoph Heiss <c.heiss@proxmox.com>
Link: https://lore.proxmox.com/20260512173145.596958-22-s.hanreich@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/sdn/fabrics/FabricEdit.js
@@ -5,7 +5,17 @@ Ext.define('PVE.sdn.Fabric.Fabric.Edit', {
     width: 400,
 
     fabricId: undefined,
+
+    hasIpv4Support: true,
+    hasIpv6Support: true,
+
     baseUrl: '/cluster/sdn/fabrics/fabric',
+
+    viewModel: {
+        data: {
+            showIpv6ForwardingHint: false,
+        },
+    },
 
     items: [
         {
@@ -24,18 +34,6 @@ Ext.define('PVE.sdn.Fabric.Fabric.Edit', {
                 disabled: '{!isCreate}',
             },
         },
-        {
-            xtype: 'proxmoxtextfield',
-            fieldLabel: gettext('IPv4 Prefix'),
-            labelWidth: 120,
-            name: 'ip_prefix',
-            allowBlank: true,
-            skipEmptyText: true,
-            cbind: {
-                disabled: '{!isCreate}',
-                deleteEmpty: '{!isCreate}',
-            },
-        },
     ],
 
     additionalItems: [],
@@ -51,6 +49,52 @@ Ext.define('PVE.sdn.Fabric.Fabric.Edit', {
             me.url = `${me.baseUrl}/${me.fabricId}`;
         } else {
             me.url = me.baseUrl;
+        }
+
+        if (me.hasIpv4Support) {
+            me.items.push({
+                xtype: 'proxmoxtextfield',
+                fieldLabel: gettext('IPv4 Prefix'),
+                labelWidth: 120,
+                name: 'ip_prefix',
+                allowBlank: true,
+                skipEmptyText: true,
+                cbind: {
+                    disabled: '{!isCreate}',
+                    deleteEmpty: '{!isCreate}',
+                },
+            });
+        }
+
+        if (me.hasIpv6Support) {
+            me.items.push(
+                {
+                    xtype: 'displayfield',
+                    value: 'To make IPv6 fabrics work, enable global IPv6 forwarding on all nodes. Click on the Help button for more details.',
+                    bind: {
+                        hidden: '{!showIpv6ForwardingHint}',
+                    },
+                    userCls: 'pmx-hint',
+                },
+                {
+                    xtype: 'proxmoxtextfield',
+                    fieldLabel: gettext('IPv6 Prefix'),
+                    labelWidth: 120,
+                    name: 'ip6_prefix',
+                    allowBlank: true,
+                    skipEmptyText: true,
+                    cbind: {
+                        disabled: '{!isCreate}',
+                        deleteEmpty: '{!isCreate}',
+                    },
+                    listeners: {
+                        change: function (textbox, value) {
+                            let vm = textbox.up('window').getViewModel();
+                            vm.set('showIpv6ForwardingHint', !!value);
+                        },
+                    },
+                },
+            );
         }
 
         me.items.push(...me.additionalItems);

www/manager6/sdn/fabrics/openfabric/FabricEdit.js
@@ -4,43 +4,11 @@ Ext.define('PVE.sdn.Fabric.OpenFabric.Fabric.Edit', {
     subject: 'OpenFabric',
     onlineHelp: 'pvesdn_openfabric_fabric',
 
-    viewModel: {
-        data: {
-            showIpv6ForwardingHint: false,
-        },
-    },
-
     extraRequestParams: {
         protocol: 'openfabric',
     },
 
     additionalItems: [
-        {
-            xtype: 'displayfield',
-            value: 'To make IPv6 fabrics work, enable global IPv6 forwarding on all nodes. Click on the Help button for more details.',
-            bind: {
-                hidden: '{!showIpv6ForwardingHint}',
-            },
-            userCls: 'pmx-hint',
-        },
-        {
-            xtype: 'proxmoxtextfield',
-            fieldLabel: gettext('IPv6 Prefix'),
-            labelWidth: 120,
-            name: 'ip6_prefix',
-            allowBlank: true,
-            skipEmptyText: true,
-            cbind: {
-                disabled: '{!isCreate}',
-                deleteEmpty: '{!isCreate}',
-            },
-            listeners: {
-                change: function (textbox, value) {
-                    let vm = textbox.up('window').getViewModel();
-                    vm.set('showIpv6ForwardingHint', !!value);
-                },
-            },
-        },
         {
             xtype: 'proxmoxintegerfield',
             // TRANSLATORS: See https://en.wikipedia.org/wiki/IS-IS#Packet_types

www/manager6/sdn/fabrics/ospf/FabricEdit.js
@@ -4,6 +4,8 @@ Ext.define('PVE.sdn.Fabric.Ospf.Fabric.Edit', {
     subject: 'OSPF',
     onlineHelp: 'pvesdn_ospf_fabric',
 
+    hasIpv6Support: false,
+
     extraRequestParams: {
         protocol: 'ospf',
     },

commit 4c424bf4f5a6549ae50162242e96d1a148cacb57

ui: sdn: fabrics: drop dead disabled bindings
The prefix fields used an invalid cbind setup for their disabled state,
so the bindings didn't work and editing the fabric prefix was possible.

Initially we didn't want the user to be able to update the prefix of a
fabric, because we would need to add a mechanism to update all the node
ips at the same time. This broke though and they became editable --
which is fine, since you still get an error "node has ip out of prefix".
Keep it this way, because editing a fabric without nodes is still
useful.

Remove the dead disabled bindings while keeping deleteEmpty handling.

Fixes: e5e3374273af ("ui: fabrics: edit: make ipv4/6 support generic over fabric panels")
Signed-off-by: Gabriel Goller <g.goller@proxmox.com>
Link: https://lore.proxmox.com/20260520125125.251655-1-g.goller@proxmox.com
 [TL: fix-up s/'{!isCreate}'/!me.isCreate/]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/manager6/sdn/fabrics/FabricEdit.js
@@ -62,10 +62,7 @@ Ext.define('PVE.sdn.Fabric.Fabric.Edit', {
                 name: 'ip_prefix',
                 allowBlank: true,
                 skipEmptyText: true,
-                cbind: {
-                    disabled: '{!isCreate}',
-                    deleteEmpty: '{!isCreate}',
-                },
+                deleteEmpty: !me.isCreate,
             });
         }
 
@@ -86,10 +83,7 @@ Ext.define('PVE.sdn.Fabric.Fabric.Edit', {
                     name: 'ip6_prefix',
                     allowBlank: true,
                     skipEmptyText: true,
-                    cbind: {
-                        disabled: '{!isCreate}',
-                        deleteEmpty: '{!isCreate}',
-                    },
+                    deleteEmpty: !me.isCreate,
                     listeners: {
                         change: function (textbox, value) {
                             let vm = textbox.up('window').getViewModel();