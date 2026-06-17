9b929746e1ec593e1080c4d4893da6faf37f44f0
Kefu Chai <k.chai@proxmox.com>
2026-06-17 19:52:29 +0800

    ui: ceph: config db: auto-show MASK column when masks are in use
    A mask narrows the scope of a config entry to a subset of daemons
    matching a qualifier such as 'class:hdd' or 'host:node1'. Without it
    visible, an entry set for 'osd/class:hdd' shows up as 'osd' in the
    table, indistinguishable from a setting that applies to all OSDs.
    
    The MASK column existed since the table was introduced but was hidden
    in f9bbe5fe to reduce noise: the field is empty for the vast majority
    of entries on typical clusters. Exposing it unconditionally would add
    an empty column for users who never set any masked options.
    
    Check after the store loads whether any entry has a non-empty mask and
    unhide the column only then. On clusters without masked settings the
    table is unaffected.
    
    Fixes #6948.
    
    Signed-off-by: Kefu Chai <k.chai@proxmox.com>
    Link: https://lore.proxmox.com/20260617115229.3763257-2-k.chai@proxmox.com

--- www/manager6/ceph/Config.js
+++ www/manager6/ceph/Config.js
@@ -62,6 +62,13 @@ Ext.define('PVE.node.CephConfigDb', {
 
         me.callParent();
 
+        me.getStore().on('load', function(store) {
+            let hasMask = store.getRange().some(rec => rec.get('mask'));
+            if (hasMask) {
+                me.down('[dataIndex=mask]').setVisible(true);
+            }
+        });
+
         Proxmox.Utils.monStoreErrors(me, me.getStore());
         me.getStore().load();
     },
